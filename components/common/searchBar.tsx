8ebbed4ac93e66b5636206c4eace4036fae387c7
Eric B <ericboysen1@gmail.com>
2022-02-23 18:34:46 -0600

    feat: update splash page design

--- components/common/searchBar.tsx
+++ components/common/searchBar.tsx
@@ -1,13 +1,26 @@
-import SearchIcon from '@material-ui/icons/Search'
+import {SearchIcon} from './searchIcon'
 import * as React from 'react';
 import { withStyles } from '@mui/material';
 import { SvgIcon } from '@mui/material';
 
+type SearchProps={
+    setSearch:Function
+}
 
-export const SearchBar = ()=>{
+
+
+export const SearchBar = (props:SearchProps)=>{
     const [count,setCount] = React.useState(0);
     return <>
-        <SearchIcon className='translate-x-8' style={{color:"6d7ec4"}}/>
-        <input type='search' className='rounded-xl border-primary-dark border-2 w-11/12 h-12 p-8 bg-white text-primary-dark placeholder-primary-dark font-semibold' placeholder='Search section number, professor name, course number....' />
+    <div className=' text-primary m-auto w-11/12 -translate-y-1/2'>
+        <div className='translate-y-10 translate-x-4 w-8 h-8'>
+            <SearchIcon />
+        </div>
+        <input type='search' 
+        id='mainSearch'
+        className='rounded-sm border-primary-dark border-2 w-full h-12 pl-12 bg-white text-primary-dark placeholder-primary-dark font-semibold' 
+        placeholder='Search section number, professor name, course number....' 
+        onKeyPress={e=> e.key==='Enter' && props.setSearch(e.currentTarget.value)}/>
+    </div>
     </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+
+      'short':{'raw': '(max-height: 200px)'},
+      'xs': '400px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         primary: {
