84f5df381a48fc2d4a0853241eabf8c13b305f18
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-12 21:18:59 -0500

    Linked logo to home page

--- components/navigation/topMenu/topMenu.tsx
+++ components/navigation/topMenu/topMenu.tsx
@@ -2,24 +2,27 @@ import { Icon } from '@mui/material';
 import { FlatLogoIcon } from '../../icons/FlatLogoIcon/flatLogoIcon';
 import HandymanIcon from '@mui/icons-material/Handyman';
 import React from 'react';
+import Link from 'next/link';
 
 /**
  * This is a component to hold UTD Trends branding and basic navigation
- * @returns 
+ * @returns
  */
-export function TopMenu(){
+export function TopMenu() {
   return (
     <div className="bg-primary-dark h-16 text-light relative p-4">
-      <div className="h-full float-left flex w-1/4 " >
-        <div className="h-full flex align-middle place-items-center justify-center" >
-          <div className="h-full float-left mr-2">
-            <FlatLogoIcon />
+      <div className="h-full float-left flex w-1/4">
+        <Link href="/" passHref>
+          <div className="h-full flex align-middle place-items-center justify-center cursor-pointer">
+            <div className="h-full float-left mr-2">
+              <FlatLogoIcon />
+            </div>
+            <h1 className=" float-right text-xl">UTD Trends</h1>
           </div>
-          <h1 className=" float-right text-xl" >UTD Trends</h1>
-        </div>
+        </Link>
       </div>
     </div>
   );
-};
+}
 
 export default TopMenu;
