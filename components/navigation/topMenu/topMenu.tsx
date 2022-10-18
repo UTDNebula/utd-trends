762a0f0092df55a47754ad660b0ff13e03f5f167
SameerIslam <islamsameer91@gmail.com>
2022-10-17 19:32:13 -0500

    nav bar linked to home page

--- components/navigation/topMenu/topMenu.tsx
+++ components/navigation/topMenu/topMenu.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@mui/material';
 import { FlatLogoIcon } from '../../icons/FlatLogoIcon/flatLogoIcon';
 import HandymanIcon from '@mui/icons-material/Handyman';
 import React from 'react';
+import Link from 'next/link';
 
 /**
  * This is a component to hold UTD Trends branding and basic navigation
@@ -11,12 +12,14 @@ export function TopMenu(){
   return (
     <div className="bg-primary-dark h-16 text-light relative p-4">
       <div className="h-full float-left flex w-1/4 " >
+        <Link href="../">
         <div className="h-full flex align-middle place-items-center justify-center" >
           <div className="h-full float-left mr-2">
             <FlatLogoIcon />
           </div>
           <h1 className=" float-right text-xl" >UTD Trends</h1>
         </div>
+        </Link>
       </div>
     </div>
   );
