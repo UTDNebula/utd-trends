c63778b42d491e2aa5a40135d5296b50f11c4519
Tyler Hill <gotyhill@gmail.com>
2023-02-23 23:44:59 -0600

    Autocomplete format add
    Added search format: (<professorLast> or <professorFirst> <professorLast>) (<prefix> <number> or <prefix><number>) to autocomplete system

--- pages/api/autocomplete.ts
+++ pages/api/autocomplete.ts
@@ -40,6 +40,8 @@ let myPrefixes: Prefix[] = [
 let myClasses: Class[] = [
   { prefix: myPrefixes[0], number: 1337, professors: [myProfessors[0]] },
   { prefix: myPrefixes[0], number: 1336, professors: [myProfessors[1]] },
+  { prefix: myPrefixes[2], number: 1337, professors: [myProfessors[0]] },
+  { prefix: myPrefixes[2], number: 1336, professors: [myProfessors[1]] },
   {
     prefix: myPrefixes[0],
     number: 2337,
@@ -48,10 +50,12 @@ let myClasses: Class[] = [
 ];
 
 myProfessors[0].classes.push(myClasses[0]);
+myProfessors[0].classes.push(myClasses[2]);
 myProfessors[1].classes.push(myClasses[1]);
+myProfessors[1].classes.push(myClasses[3])
 
 myPrefixes[0].classes.push(myClasses[0], myClasses[1]);
-myPrefixes[2].classes.push(myClasses[0], myClasses[1]);
+myPrefixes[2].classes.push(myClasses[2], myClasses[3]);
 
 type NodeAttributes = {
   character: string;
@@ -112,11 +116,10 @@ for (let i = 0; i < myPrefixes.length; i++) {
   const prefixNode = addSearchQueryCharacter(root, myPrefixes[i].value, {
     prefix: myPrefixes[i].value,
   });
-  const prefixSpaceNode = graph.addNode(numNodes++, {
-    character: ' ',
-    visited: false,
-  });
-  graph.addEdge(prefixNode, prefixSpaceNode);
+  const prefixSpaceNode = addSearchQueryCharacter(
+    prefixNode,
+    ' ',
+  );
   for (let j = 0; j < myPrefixes[i].classes.length; j++) {
     //console.log(myPrefixes[i].classes[j].number);
     const classNode = addSearchQueryCharacter(
@@ -127,11 +130,10 @@ for (let i = 0; i < myPrefixes.length; i++) {
         number: myPrefixes[i].classes[j].number,
       },
     );
-    const classSpaceNode = graph.addNode(numNodes++, {
-      character: ' ',
-      visited: false,
-    });
-    graph.addEdge(classNode, classSpaceNode);
+    const classSpaceNode = addSearchQueryCharacter(
+      classNode,
+      ' ',
+    );
     for (let k = 0; k < myPrefixes[i].classes[j].professors.length; k++) {
       //console.log(myPrefixes[i].classes[j].professors[k].firstName + myPrefixes[i].classes[j].professors[k].lastName);
       //class -> first name -> last name
@@ -159,12 +161,65 @@ for (let i = 0; i < myPrefixes.length; i++) {
       );
     }
   }
-  console.log(myPrefixes[i].value);
   graph.forEachOutNeighbor(prefixSpaceNode, child => { //support no space between prefix and number
-    console.log(graph.getNodeAttribute(child, 'character'));
     graph.addEdge(prefixNode, child);
   });
 }
+
+//Add nodes in format: (<professorLast> or <professorFirst> <professorLast>) (<prefix> <number> or <prefix><number>)
+for (let i = 0; i < myProfessors.length; i++) {
+  console.log(myProfessors[i].firstName + ' ' + myProfessors[i].lastName);
+  //root -> first name -> last name
+  //root -> last name
+  const professorFirstNameNode = addSearchQueryCharacter(
+    root,
+    myProfessors[i].firstName + ' ',
+  );
+  const professorLastNameFirstCharNode = addSearchQueryCharacter(
+    root,
+    myProfessors[i].lastName[0],
+  );
+  graph.addEdge(professorFirstNameNode, professorLastNameFirstCharNode);
+  const professorLastNameNode = addSearchQueryCharacter(
+    professorLastNameFirstCharNode,
+    myProfessors[i].lastName.slice(1) + ' ',
+    {
+      professorName:
+        myProfessors[i].firstName +
+        ' ' +
+        myProfessors[i].lastName,
+    },
+  );
+  for (let j = 0; j < myProfessors[i].classes.length; j++) {
+    console.log(myProfessors[i].classes[j].prefix.value + ' ' + myProfessors[i].classes[j].number);
+    const prefixNode = addSearchQueryCharacter(
+      professorLastNameNode,
+      myProfessors[i].classes[j].prefix.value,
+    );
+    const prefixSpaceNode = addSearchQueryCharacter(
+      prefixNode,
+      ' ',
+    );
+    const classNodeFirstChar = addSearchQueryCharacter(
+      prefixSpaceNode,
+      myProfessors[i].classes[j].number.toString()[0],
+    );
+    graph.addEdge(prefixNode, classNodeFirstChar);
+    const classNode = addSearchQueryCharacter(
+      classNodeFirstChar,
+      myProfessors[i].classes[j].number.toString().slice(1),
+      {
+        prefix: myProfessors[i].classes[j].prefix.value,
+        number: myProfessors[i].classes[j].number,
+        professorName:
+          myProfessors[i].firstName +
+          ' ' +
+          myProfessors[i].lastName,
+      },
+    );
+  }
+}
+
 
 /*reduces graph size by compressing chains of nodes each with only one child
 to a single node with a character value of several characters. I couldn't get
@@ -201,11 +256,12 @@ checkForSingleChild(root);*/
 type QueueItem = {
   node: string;
   characters?: string;
-  matched: boolean;
+  toNext: boolean;
 };
 
 function bfsToNextData(queue: QueueItem[]) {
   const queueItem = queue.shift();
+  //console.log(graph.getNodeAttribute(queueItem?.node, 'character'));
   if (graph.getNodeAttribute(queueItem?.node, 'visited')) {
     return;
   }
@@ -217,7 +273,7 @@ function bfsToNextData(queue: QueueItem[]) {
     graph.forEachOutNeighbor(queueItem?.node, (neighbor) => {
       queue.push({
         node: neighbor,
-        matched: false,
+        toNext: true,
       });
     });
   }
@@ -226,43 +282,32 @@ function bfsToNextData(queue: QueueItem[]) {
 
 function bfsRecursion(queue: QueueItem[]) {
   const queueItem = queue.shift();
-  const data = graph.getNodeAttribute(queueItem?.node, 'data');
-  const hasData = typeof data !== 'undefined';
-  const lastChar = queueItem?.characters?.length === 1;
   if (
     queueItem?.characters?.[0] ===
     graph.getNodeAttribute(queueItem?.node, 'character')
-  ) {
-    //match
-    if (lastChar) {
+  ) { //match
+    //console.log('match: ', queueItem?.characters, queueItem?.characters?.length === 1);
+    if (queueItem?.characters?.length === 1) { //last character
       graph.forEachOutNeighbor(queueItem?.node, (neighbor) => {
+        //console.log('toNext: ', graph.getNodeAttribute(neighbor, 'character'));
         queue.push({
           node: neighbor,
-          matched: false,
+          toNext: true,
         });
       });
-      if (hasData) {
+      const data = graph.getNodeAttribute(queueItem?.node, 'data');
+      if (typeof data !== 'undefined') { //has data
         return data;
       }
     } else {
       graph.forEachOutNeighbor(queueItem?.node, (neighbor) => {
+        //console.log('queue: ', graph.getNodeAttribute(neighbor, 'character'));
         queue.push({
           node: neighbor,
           characters: queueItem?.characters?.slice(1),
-          matched: true,
+          toNext: false,
         });
       });
-    }
-  } else {
-    if (lastChar) {
-      if (!hasData) {
-        graph.forEachOutNeighbor(queueItem?.node, (neighbor) => {
-          queue.push({
-            node: neighbor,
-            matched: false,
-          });
-        });
-      }
     }
   }
 }
@@ -281,16 +326,16 @@ function bfs(node: string, characters: string) {
     queue.push({
       node: neighbor,
       characters: characters,
-      matched: true,
+      toNext: false,
     });
   });
   let results: SearchQuery[] = [];
   while (queue.length) {
     let response: bfsReturn;
-    if (queue[0].matched) {
-      response = bfsRecursion(queue);
-    } else {
+    if (queue[0].toNext) {
       response = bfsToNextData(queue);
+    } else {
+      response = bfsRecursion(queue);
     }
     if (typeof response !== 'undefined') {
       results.push(response);
@@ -325,7 +370,15 @@ export default function handler(
     console.log('CS 1336 C', bfs(root, 'CS 1336 C'));
     console.log('CS 1336 A', bfs(root, 'CS 1336 A'));
     console.log('CS 1336 CS', bfs(root, 'CS 1336 CS'));
-    console.log('CS 1336 AC', bfs(root, 'CS 1336 AC'));*/
+    console.log('CS 1336 AC', bfs(root, 'CS 1336 AC'));
+    console.log('A', bfs(root, 'A'));
+    console.log('T', bfs(root, 'T'));
+    console.log('AR', bfs(root, 'AR'));
+    console.log('ARCS', bfs(root, 'ARCS'));
+    console.log('TEST', bfs(root, 'TEST'));
+    console.log('TEST ', bfs(root, 'TEST '));
+    console.log('TEST CS1', bfs(root, 'TEST CS1'));
+    console.log('TEST CS 1', bfs(root, 'TEST CS 1'));*/
     res.status(200).json({ data: bfs(root, parsedInput) });
   } else {
     res.status(400).json({ error: 'No input, or invalid type of input' });
