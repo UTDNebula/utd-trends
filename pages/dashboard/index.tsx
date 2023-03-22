9f42595e9a6af250536826ba0331d6a46f4558dd
William Skaggs <wskaggs3262@gmail.com>
2023-03-22 17:54:26 -0500

    Separate RateMyProfessor querying from the Nebula API grades route querying, so an error in one does not hide the other

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -47,11 +47,16 @@ export const Dashboard: NextPage = () => {
       wouldTakeAgainPercentage: 88,
     },
   ]);
+
+  const [professorInvolvingSearchTerms, setProfessorInvolvingSearchTerms] =
+    useState<SearchQuery[]>([]);
+
   function round(val: number) {
     return Math.round((val + Number.EPSILON) * 100) / 100;
   }
   const router = useRouter();
-  const [state, setState] = useState('loading');
+  const [gradesState, setGradesState] = useState('loading');
+  const [professorRatingsState, setProfessorRatingsState] = useState('loading');
 
   //load data from home search if present
   const startingData: SearchQuery = {};
@@ -84,22 +89,40 @@ export const Dashboard: NextPage = () => {
   }
 
   useEffect(() => {
-    fetch('/api/ratemyprofessorScraper?professors=Greg%20Ozbirn,John%20Cole', {
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setProfData(data);
+    if (professorInvolvingSearchTerms.length > 0) {
+      setProfessorRatingsState('loading');
+      let call: string = '/api/ratemyprofessorScraper?professors=';
+      professorInvolvingSearchTerms.forEach((searchTerm) => {
+        // @ts-ignore
+        call += encodeURIComponent(searchTerm.professorName) + ',';
       });
-  }, [dat]);
+      fetch(call.substring(0, call.length - 2), {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setProfData(data);
+          setProfessorRatingsState('success');
+        })
+        .catch((error) => {
+          setProfessorRatingsState('error');
+          console.log(error);
+        });
+    } else {
+      setProfessorRatingsState('success');
+      setProfData([]);
+    }
+  }, [professorInvolvingSearchTerms]);
 
   //called from expandableSearchGrid when data being compared is changed
   function searchTermsChange(searchTerms: SearchQuery[]) {
+    setProfessorInvolvingSearchTerms(
+      searchTerms.filter(
+        (searchQuery) => searchQuery.professorName != undefined,
+      ),
+    );
     console.log('Index search terms: ', searchTerms);
-    /*if (searchTerms.length === 0) {
-      setState('loading');
-    }*/
     Promise.all(
       searchTerms.map((searchTerm: SearchQuery) => {
         let apiRoute = '';
@@ -177,13 +200,13 @@ export const Dashboard: NextPage = () => {
         console.log('data from grid: ', responses);
       })
       .catch((error) => {
-        setState('error');
+        setGradesState('error');
         console.error('Nebula API Error', error);
       });
   }
 
   useEffect(() => {
-    setState('loading');
+    setGradesState('loading');
     if (!router.isReady) return;
     if (
       'sections' in router.query &&
@@ -252,191 +275,205 @@ export const Dashboard: NextPage = () => {
           setAverageDat(newAverageDat);
           setStdevDat(newStdevDat);
 
-          setState('success');
+          setGradesState('success');
         })
         .catch((error) => {
-          setState('error');
+          setGradesState('error');
           console.error('Nebula API Error', error);
         });
     }
   }, [router.isReady, router.query.sections]);
 
-  if (state === 'loading') {
-    return (
+  let mainGradesPage;
+  let detailedGradesPage;
+  let professorRatingsPage;
+
+  if (gradesState === 'loading') {
+    mainGradesPage = (
       <>
-        <div className=" w-full bg-light h-full">
-          <TopMenu />
-          <ExpandableSearchGrid
-            onChange={searchTermsChange}
-            startingData={startingData}
-          />
-          <div className="w-full h-5/6 justify-center">
-            <div className="w-full h-5/6 relative min-h-full">
-              <Carousel>
-                <div className="h-full m-4 ">
-                  <LinearProgress className="mt-8 pt-2"></LinearProgress>
-                </div>
-                <div className="h-full m-4">
-                  <LinearProgress className="mt-8 pt-2"></LinearProgress>
-                </div>
-                <div className="h-full m-4">
-                  <LinearProgress className="mt-8 pt-2"></LinearProgress>
-                </div>
-              </Carousel>
-            </div>
-          </div>
+        <div className="h-full m-4">
+          <LinearProgress className="mt-8 pt-2"></LinearProgress>
         </div>
       </>
     );
-  } else if (state === 'error') {
-    return (
+    detailedGradesPage = (
       <>
-        <div className=" w-full bg-light h-full">
-          <TopMenu />
-          <ExpandableSearchGrid
-            onChange={searchTermsChange}
-            startingData={startingData}
-          />
-          <div className="w-full h-5/6 justify-center">
-            <div className="w-full h-5/6 relative min-h-full">
-              <Carousel>
-                <div className="h-full m-4 ">
-                  <h1 className="text-3xl text-center text-gray-600 font-semibold">
-                    An error occurred! Please reload the page, and if this
-                    problem persists, contact Nebula Labs.
-                  </h1>
-                </div>
-                <div className="h-full m-4">
-                  <h1 className="text-3xl text-center text-gray-600 font-semibold">
-                    An error occurred! Please reload the page, and if this
-                    problem persists, contact Nebula Labs.
-                  </h1>
-                </div>
-                <div className="h-full m-4">
-                  <h1 className="text-3xl text-center text-gray-600 font-semibold">
-                    An error occurred! Please reload the page, and if this
-                    problem persists, contact Nebula Labs.
-                  </h1>
-                </div>
-              </Carousel>
-            </div>
-          </div>
+        <div className="h-full m-4">
+          <LinearProgress className="mt-8 pt-2"></LinearProgress>
+        </div>
+      </>
+    );
+  } else if (gradesState === 'error') {
+    mainGradesPage = (
+      <>
+        <div className="h-full m-4">
+          <h1 className="text-3xl text-center text-gray-600 font-semibold">
+            An error occurred! Please reload the page, and if this problem
+            persists, contact Nebula Labs.
+          </h1>
+        </div>
+      </>
+    );
+    detailedGradesPage = (
+      <>
+        <div className="h-full m-4">
+          <h1 className="text-3xl text-center text-gray-600 font-semibold">
+            An error occurred! Please reload the page, and if this problem
+            persists, contact Nebula Labs.
+          </h1>
         </div>
       </>
     );
   } else {
-    return (
+    mainGradesPage = (
       <>
-        <div className=" w-full bg-light h-full">
-          <TopMenu />
-          <ExpandableSearchGrid
-            onChange={searchTermsChange}
-            startingData={startingData}
-          />
-          <div className="w-full h-5/6 justify-center">
-            <div className="w-full h-5/6 relative min-h-full">
-              <Carousel>
-                <div className="h-full m-4 ">
-                  <Card className="h-96 p-4 m-4">
-                    <GraphChoice
-                      form="Bar"
-                      title="Grades"
-                      xaxisLabels={[
-                        'A+',
-                        'A',
-                        'A-',
-                        'B+',
-                        'B',
-                        'B-',
-                        'C+',
-                        'C',
-                        'C-',
-                        'D+',
-                        'D',
-                        'D-',
-                        'F',
-                        'W',
-                      ]}
-                      series={dat}
-                    />
-                  </Card>
-                </div>
-                <div className="p-4 h-full">
-                  <Card className="h-96 p-4 m-4">
-                    <GraphChoice
-                      form="BoxWhisker"
-                      title="GPA Box and Whisker"
-                      xaxisLabels={[
-                        'A+',
-                        'A',
-                        'A-',
-                        'B+',
-                        'B',
-                        'B-',
-                        'C+',
-                        'C',
-                        'C-',
-                        'D+',
-                        'D',
-                        'D-',
-                        'F',
-                        'W',
-                      ]}
-                      series={GPAdat}
-                    />
-                  </Card>
-                  <div className="grid grid-cols-1 md:grid-cols-2">
-                    <Card className="h-96 p-4 m-4">
-                      <GraphChoice
-                        form="Bar"
-                        title="GPA Averages"
-                        xaxisLabels={['Average']}
-                        series={averageDat}
-                      />
-                    </Card>
-                    <Card className="h-96 p-4 m-4">
-                      <GraphChoice
-                        form="Bar"
-                        title="GPA Standard Deviations"
-                        xaxisLabels={['Standard Deviation']}
-                        series={stdevDat}
-                      />
-                    </Card>
-                  </div>
-                </div>
-
-                <div className="p-4 h-full">
-                  {profData.length > 0 ? (
-                    profData.map((data: RMPData, index: number) => (
-                      <Card className="h-fit m-4" key={index}>
-                        <ProfessorCard
-                          position="relative"
-                          element="Card"
-                          professorRating={data.averageRating}
-                          averageDifficulty={data.averageDifficulty}
-                          takingAgain={data.wouldTakeAgainPercentage}
-                          numRatings={data.numRatings}
-                          name={data.firstName + ' ' + data.lastName}
-                          department={data.department}
-                          key={index}
-                        />
-                      </Card>
-                    ))
-                  ) : (
-                    <h1 className="text-3xl text-center text-gray-600 font-semibold">
-                      No professors selected! Search for a professor or one of
-                      their courses/sections in the search bar to see data about
-                      that professors specifically.
-                    </h1>
-                  )}
-                </div>
-              </Carousel>
-            </div>
+        <div className="h-full m-4">
+          <Card className="h-96 p-4 m-4">
+            <GraphChoice
+              form="Bar"
+              title="Grades"
+              xaxisLabels={[
+                'A+',
+                'A',
+                'A-',
+                'B+',
+                'B',
+                'B-',
+                'C+',
+                'C',
+                'C-',
+                'D+',
+                'D',
+                'D-',
+                'F',
+                'W',
+              ]}
+              series={dat}
+            />
+          </Card>
+        </div>
+      </>
+    );
+    detailedGradesPage = (
+      <>
+        <div className="h-full m-4">
+          <Card className="h-96 p-4 m-4">
+            <GraphChoice
+              form="BoxWhisker"
+              title="GPA Box and Whisker"
+              xaxisLabels={[
+                'A+',
+                'A',
+                'A-',
+                'B+',
+                'B',
+                'B-',
+                'C+',
+                'C',
+                'C-',
+                'D+',
+                'D',
+                'D-',
+                'F',
+                'W',
+              ]}
+              series={GPAdat}
+            />
+          </Card>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <Card className="h-96 p-4 m-4">
+              <GraphChoice
+                form="Bar"
+                title="GPA Averages"
+                xaxisLabels={['Average']}
+                series={averageDat}
+              />
+            </Card>
+            <Card className="h-96 p-4 m-4">
+              <GraphChoice
+                form="Bar"
+                title="GPA Standard Deviations"
+                xaxisLabels={['Standard Deviation']}
+                series={stdevDat}
+              />
+            </Card>
           </div>
         </div>
       </>
     );
   }
+
+  if (professorRatingsState === 'loading') {
+    professorRatingsPage = (
+      <>
+        <div className="h-full m-4">
+          <LinearProgress className="mt-8 pt-2"></LinearProgress>
+        </div>
+      </>
+    );
+  } else if (professorRatingsState === 'error') {
+    professorRatingsPage = (
+      <>
+        <div className="h-full m-4">
+          <h1 className="text-3xl text-center text-gray-600 font-semibold">
+            An error occurred! Please reload the page, and if this problem
+            persists, contact Nebula Labs.
+          </h1>
+        </div>
+      </>
+    );
+  } else {
+    professorRatingsPage = (
+      <>
+        <div className="h-full m-4">
+          {profData.length > 0 ? (
+            profData.map((data: RMPData, index: number) => (
+              <Card className="h-fit m-4" key={index}>
+                <ProfessorCard
+                  position="relative"
+                  element="Card"
+                  professorRating={data.averageRating}
+                  averageDifficulty={data.averageDifficulty}
+                  takingAgain={data.wouldTakeAgainPercentage}
+                  numRatings={data.numRatings}
+                  name={data.firstName + ' ' + data.lastName}
+                  department={data.department}
+                  key={index}
+                />
+              </Card>
+            ))
+          ) : (
+            <h1 className="text-3xl text-center text-gray-600 font-semibold">
+              No professors selected! Search for a professor or one of their
+              courses/sections in the search bar to see data about that
+              professor.
+            </h1>
+          )}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className=" w-full bg-light h-full">
+        <TopMenu />
+        <ExpandableSearchGrid
+          onChange={searchTermsChange}
+          startingData={startingData}
+        />
+        <div className="w-full h-5/6 justify-center">
+          <div className="w-full h-5/6 relative min-h-full">
+            <Carousel>
+              {mainGradesPage}
+              {detailedGradesPage}
+              {professorRatingsPage}
+            </Carousel>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
