85b2cc7df82cf8b8206b722d4f14554fcf0c5ed4
Tyler Hill <gotyhill@gmail.com>
2023-04-09 17:19:26 -0500

    Add share button

--- components/navigation/topMenu/topMenu.tsx
+++ components/navigation/topMenu/topMenu.tsx
@@ -1,19 +1,22 @@
-import { Icon } from '@mui/material';
+import { IconButton } from '@mui/material';
 import { LogoIcon } from '../../icons/LogoIcon/logoIcon';
-import HandymanIcon from '@mui/icons-material/Handyman';
+import { Share } from '@mui/icons-material';
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 /**
  * This is a component to hold UTD Trends branding and basic navigation
  * @returns
  */
 export function TopMenu() {
+  const router = useRouter();
+
   return (
-    <div className="bg-primary h-16 text-light relative p-4">
-      <div className="h-full float-left flex min-w-fit w-1/4 justify-center ">
+    <div className="bg-primary h-16 text-light relative py-2 px-4">
+      <div className="h-full flex min-w-fit justify-between">
         <Link href="/">
-          <a>
+          <a className="my-2 mx-2">
             <div className="h-full flex align-middle place-items-center justify-center">
               <div className="h-full float-left mr-2 w-7">
                 <LogoIcon />
@@ -22,6 +25,32 @@ export function TopMenu() {
             </div>
           </a>
         </Link>
+        <IconButton
+          className="w-12"
+          size="large"
+          onClick={() => {
+            let url = router.asPath;
+            if (
+              router.query &&
+              Object.keys(router.query).length === 0 &&
+              Object.getPrototypeOf(router.query) === Object.prototype
+            ) {
+              url = 'https://trends.utdnebula.com/';
+            }
+            if (navigator.share) {
+              navigator.share({
+                title: 'UTD Trends',
+                url: url,
+              });
+            } else if (navigator.clipboard) {
+              navigator.clipboard.writeText(url);
+            } else {
+              alert(url);
+            }
+          }}
+        >
+          <Share className="fill-primary-dark text-3xl mr-1" />
+        </IconButton>
       </div>
     </div>
   );
