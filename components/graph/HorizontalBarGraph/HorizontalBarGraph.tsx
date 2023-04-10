220d949715316824a85b4add695df3f622ebf5b4
Tyler Hill <gotyhill@gmail.com>
2023-04-09 19:21:03 -0500

    Move axis label formatter for horizontal bar graph

--- components/graph/HorizontalBarGraph/HorizontalBarGraph.tsx
+++ components/graph/HorizontalBarGraph/HorizontalBarGraph.tsx
@@ -28,8 +28,6 @@ export function HorizontalBarGraph(props: GraphProps) {
     },
     xaxis: {
       categories: props.xaxisLabels,
-    },
-    yaxis: {
       labels: {
         formatter: props.yaxisFormatter,
       },
