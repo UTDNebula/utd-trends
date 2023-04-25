0f36c4d474ef66ce97925c1658ad56e632989ddd
Tyler Hill <gotyhill@gmail.com>
2023-04-25 17:38:50 -0500

    process.env test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Box, IconButton, Card, Modal, Fade } from '@mui/material';
+import GitHub from '@mui/icons-material/GitHub';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [gitInfoOpen, setGitInfoOpen] = useState<boolean>(false);
+  const showGitInfo =
+    typeof process.env.NEXT_PUBLIC_VERCEL_COMMIT_SHA !== 'undefined' &&
+    typeof process.env.NODE_ENV !== 'undefined' &&
+    typeof process.env.NEXT_PUBLIC_VERCEL_ENV !== 'undefined' &&
+    true;
+
   return (
     <>
       <Head>
@@ -12,6 +22,42 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
       <Analytics />
+      {showGitInfo ? (
+        <>
+          <Box className="w-fit h-fit bg-light fixed bottom-2 right-2 rounded-full">
+            <IconButton
+              size="large"
+              onClick={() => {
+                setGitInfoOpen(true);
+              }}
+            >
+              <GitHub className="fill-dark text-3xl" />
+            </IconButton>
+          </Box>
+          <Modal
+            open={gitInfoOpen}
+            onClose={() => setGitInfoOpen(false)}
+            className="flex justify-center align-center"
+          >
+            <Fade in={gitInfoOpen}>
+              <Card className="p-4 m-12 h-fit">
+                <a
+                  target="_blank"
+                  className="commit-link"
+                  href={
+                    'https://github.com/UTDNebula/utd-trends/commit/' +
+                    process.env.NEXT_PUBLIC_VERCEL_COMMIT_SHA
+                  }
+                >
+                  {process.env.NEXT_PUBLIC_VERCEL_COMMIT_SHA.substring(0, 7)}
+                </a>
+                {process.env.NODE_ENV}
+                {process.env.NEXT_PUBLIC_VERCEL_ENV}
+              </Card>
+            </Fade>
+          </Modal>
+        </>
+      ) : null}
     </>
   );
 }
