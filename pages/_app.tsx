e8e823769277056c2bb55592b145a2910c27a5dc
Tyler Hill <gotyhill@gmail.com>
2023-04-27 19:51:54 -0500

    Styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,17 @@ import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Box, IconButton, Card, Modal, Fade } from '@mui/material';
+import { IconButton, Card } from '@mui/material';
 import GitHub from '@mui/icons-material/GitHub';
 import { useState } from 'react';
+import { useMediaQuery } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [gitInfoOpen, setGitInfoOpen] = useState<boolean>(false);
   const showGitInfo =
     typeof process.env.NEXT_PUBLIC_VERCEL_ENV !== 'undefined' &&
     process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const darkModeElevation = prefersDarkMode ? 3 : 1;
 
   return (
     <>
@@ -20,38 +22,30 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Component {...pageProps} />
       <Analytics />
-      {showGitInfo ? (
+      {showGitInfo || true ? (
         <>
-          <Box className="w-fit h-fit bg-light fixed bottom-2 right-2 rounded-full">
-            <IconButton
-              size="large"
-              onClick={() => {
-                setGitInfoOpen(true);
-              }}
-            >
-              <GitHub className="fill-dark text-3xl" />
-            </IconButton>
-          </Box>
-          <Modal
-            open={gitInfoOpen}
-            onClose={() => setGitInfoOpen(false)}
-            className="flex justify-center align-center"
+          <Card
+            className="w-fit h-fit bg-light fixed bottom-2 right-2 rounded-full"
+            elevation={darkModeElevation}
           >
-            <Fade in={gitInfoOpen}>
-              <Card className="p-4 m-12 h-fit">
-                <a
-                  href={
-                    'https://github.com/UTDNebula/utd-trends/commit/' +
-                    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
-                  }
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7)}
-                </a>
-              </Card>
-            </Fade>
-          </Modal>
+            <a
+              href={
+                'https://github.com/UTDNebula/utd-trends/commit/' +
+                process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
+              }
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <IconButton
+                size="large"
+                onClick={() => {
+                  setGitInfoOpen(true);
+                }}
+              >
+                <GitHub className="fill-dark text-3xl" />
+              </IconButton>
+            </a>
+          </Card>
         </>
       ) : null}
     </>
