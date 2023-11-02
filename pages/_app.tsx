0e3c4b5e8fe985d6b56243158c4668ef41658635
Tyler Hill <gotyhill@gmail.com>
2023-11-02 14:59:12 -0500

    Update icon color and fallback to png before ico

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>UTD Trends</title>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="icon" href="/logoIcon.svg" type="image/svg+xml" />
       </Head>
       <ThemeProvider theme={muiTheme}>
