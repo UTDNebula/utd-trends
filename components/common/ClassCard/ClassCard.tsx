b65463f847bfe50c8fe0986842cc61bb53087a31
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2024-02-27 00:14:29 -0600

    added stuff --  overall, grading & Most frequent grade boxes

--- components/common/ClassCard/ClassCard.tsx
+++ components/common/ClassCard/ClassCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Box,Card } from '@mui/material';
 import React from 'react';
 
 import GraphProps from '../../../modules/GraphProps/GraphProps';
@@ -18,6 +18,22 @@ export const ClassCard = (props: ClassCardProps) => {
       <div className="h-80">
         <BarGraph {...props.graphProps} />
       </div>
+      <Box className="bg-gray-800 p-4 ml-5 mr-1">
+        <div className="text-left inline-block m-0 w-1/3 pl-5">
+          <h1>Overall</h1>
+          <h1 className="text-2xl">4.5</h1>
+        </div>
+
+        <div className="text-Sleft inline-block m-0  w-1/3 pl-5">
+          <h1>Grading</h1>
+          <h1 className="text-2xl">4.5</h1>
+        </div>
+
+        <div className="text-left inline-block m-0  w-1/3 pl-5">
+          <h1>Most frequent grade</h1>
+          <h1 className="text-2xl">4.5</h1>
+        </div>
+      </Box>
     </Card>
   );
 };
