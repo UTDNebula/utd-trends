7ebe40d6d54781671ae64996981a7e8cecf044bc
Tyler Hill <gotyhill@gmail.com>
2024-02-28 15:56:02 -0600

    Loading animation and mobile responsiveness

--- components/common/ClassCard/ClassCard.tsx
+++ components/common/ClassCard/ClassCard.tsx
@@ -1,5 +1,5 @@
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Box, Card, Collapse, IconButton } from '@mui/material';
+import { Card, Collapse, IconButton, Skeleton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import fetchWithCache, {
@@ -17,8 +17,6 @@ interface ClassCardProps {
 }
 
 export const ClassCard = (props: ClassCardProps) => {
-  const label = searchQueryLabel(props.searchQuery);
-
   const [open, setOpen] = useState(
     typeof props.open === 'undefined' ? false : props.open,
   );
@@ -28,12 +26,13 @@ export const ClassCard = (props: ClassCardProps) => {
     grade_distribution: number[];
   }
   //grade data storage
-  const [data, setData] = useState<GradeData[]>([]);
+  const [gradeData, setGradeData] = useState<GradeData[]>([]);
   interface GradeResponse {
     message: string;
     data: GradeData[];
   }
   [];
+  const [loadingGrades, setLoadingGrades] = useState(true);
   //fetch grade data
   useEffect(() => {
     fetchWithCache(
@@ -69,14 +68,15 @@ export const ClassCard = (props: ClassCardProps) => {
             .filter((item, index, array) => array.indexOf(item) == index),
         );
       }
-      setData(data.data);
+      setGradeData(data.data);
+      setLoadingGrades(false);
     });
   }, []);
 
   //parse grade data
   const series = Array(14).fill(0);
   //sum relevant semesters
-  data.forEach((semester) => {
+  gradeData.forEach((semester) => {
     if (
       typeof props.semesters === 'undefined' ||
       props.semesters.includes(semester._id)
@@ -107,77 +107,120 @@ export const ClassCard = (props: ClassCardProps) => {
     series[i] = (series[i] / total) * 100;
   }
 
+  //evaluation storage
+  const [evalData, setEvalData] = useState<number[]>([]);
+  const [loadingEvals, setLoadingEvals] = useState(true);
+  //fetch evaluation data
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setEvalData([1, 2, 3]);
+      setLoadingEvals(false);
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  const label = searchQueryLabel(props.searchQuery);
+
   return (
     <Card className="m-4">
       <button
-        className="w-full pt-4 px-4 pb-2 flex gap-2 items-start"
+        className="w-full pt-4 px-4 pb-2 flex gap-2"
         onClick={() => setOpen((old) => !old)}
       >
-        <div className="flex gap-2 flex-wrap">
-          <p>{label}</p>
+        <div className="flex gap-2 flex-wrap self-center">
+          <p className="text-xl">{label}</p>
           <div>Add compare button here</div>
         </div>
-        <IconButton className="ml-auto">
+        <IconButton className="ml-auto self-start">
           <ExpandMoreIcon
             className={'transition-transform' + (open ? ' rotate-180' : '')}
           />
         </IconButton>
       </button>
       <div className="mx-4 mb-4">
-        <Collapse in={open} className="w-full">
-          <div className="h-80 w-full">
-            <BarGraph
-              title="Grades"
-              xaxisLabels={[
-                'A+',
-                'A',
-                'A-',
-                'B+',
-                'B',
-                'B-',
-                'C+',
-                'C',
-                'C-',
-                'D+',
-                'D',
-                'D-',
-                'F',
-                'W',
-              ]}
-              yaxisFormatter={(value) => Number(value).toFixed(0) + '%'}
-              series={[
-                {
-                  name: label,
-                  data: series,
-                },
-              ]}
-            />
-          </div>
+        <Collapse
+          in={open}
+          className={'w-full transition-all' + (open ? ' mb-2' : '')}
+        >
+          {loadingGrades ? (
+            <Skeleton variant="rounded" className="h-80 w-full" />
+          ) : (
+            <div className="h-80 w-full">
+              <BarGraph
+                title="Grades"
+                xaxisLabels={[
+                  'A+',
+                  'A',
+                  'A-',
+                  'B+',
+                  'B',
+                  'B-',
+                  'C+',
+                  'C',
+                  'C-',
+                  'D+',
+                  'D',
+                  'D-',
+                  'F',
+                  'W',
+                ]}
+                yaxisFormatter={(value) => Number(value).toFixed(0) + '%'}
+                series={[
+                  {
+                    name: label,
+                    data: series,
+                  },
+                ]}
+              />
+            </div>
+          )}
         </Collapse>
-        <div className="bg-gray-200 dark:bg-gray-800 rounded p-4 flex flex-wrap justify-around ">
+        <div className="mb-2 bg-gray-200 dark:bg-gray-800 rounded p-4 flex flex-wrap justify-around">
           <p>
-            Grades: <b>{total}</b>
+            Grades:{' '}
+            {loadingGrades ? (
+              <Skeleton variant="text" className="inline-block w-[5ch]" />
+            ) : (
+              <b>{total}</b>
+            )}
           </p>
           <p>
-            GPA: <b>{gpa}</b>
+            GPA:{' '}
+            {loadingGrades ? (
+              <Skeleton variant="text" className="inline-block w-[4ch]" />
+            ) : (
+              <b>{gpa}</b>
+            )}
           </p>
         </div>
-        <Box className="bg-gray-200 dark:bg-gray-800 p-4">
-          <div className="text-left inline-block m-0 w-1/3 pl-5">
+        <div className="bg-gray-200 dark:bg-gray-800 rounded p-4 grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 gap-2">
+          <div className="flex gap-2 items-center md:block">
             <p>Overall</p>
-            <p className="text-2xl">4.5</p>
+            {loadingEvals ? (
+              <Skeleton variant="text" className="text-2xl w-[3ch]" />
+            ) : (
+              <p className="text-2xl">{evalData[0]}</p>
+            )}
           </div>
 
-          <div className="text-Sleft inline-block m-0  w-1/3 pl-5">
+          <div className="flex gap-2 items-center md:block">
             <p>Grading</p>
-            <p className="text-2xl">4.5</p>
+            {loadingEvals ? (
+              <Skeleton variant="text" className="text-2xl w-[3ch]" />
+            ) : (
+              <p className="text-2xl">{evalData[1]}</p>
+            )}
           </div>
 
-          <div className="text-left inline-block m-0  w-1/3 pl-5">
+          <div className="flex gap-2 items-center md:block">
             <p>Most frequent grade</p>
-            <p className="text-2xl">4.5</p>
+            {loadingEvals ? (
+              <Skeleton variant="text" className="text-2xl w-[3ch]" />
+            ) : (
+              <p className="text-2xl">{evalData[2]}</p>
+            )}
           </div>
-        </Box>
+        </div>
       </div>
     </Card>
   );
