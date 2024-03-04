c817f024f29b41cda55c14bfc4e6d5088655437e
Tyler Hill <gotyhill@gmail.com>
2024-03-04 11:49:52 -0600

    Revert "Added a button for compare, some alignment, and added course div and box for course sections under graph"
    This reverts commit ad29b1647c09f45aae47780ee07ba9308509d675.

--- components/common/ClassCard/ClassCard.tsx
+++ components/common/ClassCard/ClassCard.tsx
@@ -1,13 +1,5 @@
-import AddIcon from '@mui/icons-material/Add';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {
-  Box,
-  Button,
-  Card,
-  Collapse,
-  IconButton,
-  Skeleton,
-} from '@mui/material';
+import { Card, Collapse, IconButton, Skeleton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import fetchWithCache, {
@@ -137,6 +129,7 @@ export const ClassCard = (props: ClassCardProps) => {
       >
         <div className="flex gap-2 flex-wrap self-center">
           <p className="text-xl">{label}</p>
+          <div>Add compare button here</div>
         </div>
         <IconButton className="ml-auto self-start">
           <ExpandMoreIcon
@@ -144,22 +137,6 @@ export const ClassCard = (props: ClassCardProps) => {
           />
         </IconButton>
       </button>
-
-      <div className="grid grid-cols-3 justify-between md:grid-rows-1 md:grid-cols-3 gap-2 ">
-        <div className="col-start-1 grid content-center">
-          <h1 className="pl-4">professor name</h1>
-        </div>
-        <div className="col-start-3 pr-4 grid justify-end ">
-          <Button
-            variant="outlined"
-            size="small"
-            startIcon={<AddIcon />}
-            className="text-white border-gray-800"
-          >
-            Select for compare
-          </Button>
-        </div>
-      </div>
       <div className="mx-4 mb-4">
         <Collapse
           in={open}
@@ -197,13 +174,6 @@ export const ClassCard = (props: ClassCardProps) => {
               />
             </div>
           )}
-
-          <h1>Course Sections</h1>
-          <div className="h-1/2">
-            <Box>
-              <h1>Course Sections boxes</h1>
-            </Box>
-          </div>
         </Collapse>
         <div className="mb-2 bg-gray-200 dark:bg-gray-800 rounded p-4 flex flex-wrap justify-around">
           <p>
