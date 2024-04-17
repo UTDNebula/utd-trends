2d10cf27396d6aaf5f4b1e194e44bbfd4f26997f
Tyler Hill <gotyhill@gmail.com>
2024-04-16 20:53:12 -0500

    Labels and styling

--- components/common/Filters/filters.tsx
+++ components/common/Filters/filters.tsx
@@ -4,28 +4,41 @@ import React from 'react';
 /**
  * This component returns a set of filters with which to sort results.
  */
+
 const Filters = () => {
   return (
     <div className="flex gap-2">
-      <FormControl fullWidth size="small">
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select label="Age" labelId="demo-simple-select-label">
+      <FormControl
+        fullWidth
+        size="small"
+        className="[&>.MuiInputBase-root]:bg-white [&>.MuiInputBase-root]:dark:bg-haiti"
+      >
+        <InputLabel id="minGPA">Minimum GPA</InputLabel>
+        <Select label="Minimum GPA" labelId="minGPA">
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
-      <FormControl fullWidth size="small">
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select label="Age" labelId="demo-simple-select-label">
+      <FormControl
+        fullWidth
+        size="small"
+        className="[&>.MuiInputBase-root]:bg-white [&>.MuiInputBase-root]:dark:bg-haiti"
+      >
+        <InputLabel id="minRating">Minimum Rating</InputLabel>
+        <Select label="Minimum Rating" labelId="minRating">
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
-      <FormControl fullWidth size="small">
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-        <Select label="Age" labelId="demo-simple-select-label">
+      <FormControl
+        fullWidth
+        size="small"
+        className="[&>.MuiInputBase-root]:bg-white [&>.MuiInputBase-root]:dark:bg-haiti"
+      >
+        <InputLabel id="maxDiff">Maximum Difficulty</InputLabel>
+        <Select label="Maximum Difficulty" labelId="maxDiff">
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
