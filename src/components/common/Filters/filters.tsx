d38a2f42496436c7f18b63a0ea02e9877cef5fe0
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2024-10-08 01:15:47 -0500

    shorter label for chosen filter semesters

--- src/components/common/Filters/filters.tsx
+++ src/components/common/Filters/filters.tsx
@@ -88,6 +88,20 @@ const Filters = ({
     );
   }
 
+  function compareSemesters(a: string, b: string) {
+    const x = a.substring(0, 2).localeCompare(b.substring(0, 2));
+    if (x == 0) {
+      const a_char = a[2];
+      const b_char = b[2];
+      // a_char and b_char cannot both be the same semester because x == 0
+      if (a_char == 'S') return -1;
+      if (a_char == 'U' && b_char == 'S') return 1;
+      if (a_char == 'U' && b_char == 'F') return -1;
+      if (a_char == 'F') return 1;
+      return 0;
+    } else return x;
+  }
+
   return (
     <div className="flex gap-2">
       {/* min GPA dropdown*/}
@@ -185,9 +199,7 @@ const Filters = ({
             if (chosenSessions.length === academicSessions.length) {
               return 'All selected';
             }
-            return selected
-              .map((session) => displayAcademicSessionName(session))
-              .join(', ');
+            return selected.sort((a, b) => compareSemesters(a, b)).join(', ');
           }}
           MenuProps={{ autoFocus: false }}
         >
