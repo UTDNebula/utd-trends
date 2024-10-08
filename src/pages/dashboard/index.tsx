701f4f09003a894e72b1048a8bad300586677715
Hersh Lalwani <Hersh.r.lalwani@gmail.com>
2024-10-07 21:13:58 -0500

    resizable RHS&LHS

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,10 @@
-import { Card, Grid } from '@mui/material';
+import { Card, Grid, useMediaQuery } from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { ImperativePanelHandle, Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { Direction } from 'react-resizable-panels/dist/declarations/src/types';
 
 import Carousel from '../../components/common/Carousel/carousel';
 import Compare from '../../components/common/Compare/compare';
@@ -673,6 +675,38 @@ export const Dashboard: NextPage = () => {
     }
   }
 
+  const panelLRef = useRef<ImperativePanelHandle>(null);
+  const panelRRef = useRef<ImperativePanelHandle>(null);
+  const panelTVert = useRef<ImperativePanelHandle>(null);
+  const isSmallScreen = useMediaQuery('(max-width: 650px)');
+  const [panelsDirection, setPanelsDirection] = useState<Direction>('horizontal');
+  // Runs when window is resized to change format
+  useEffect(function mount() {
+    window.addEventListener('resize', handleWhenResized);
+    return () => {window.removeEventListener("resize", handleWhenResized)};
+  })
+  // Runs once to decide mobile or desktop layout for initial load
+  useEffect(() => {
+    let ignore = false;
+    if (!ignore) handleWhenResized()
+    return () => { ignore = true; }
+  });
+  // Changing using useMediaQuery when window size is >650px or <650px
+  const handleWhenResized = () => {
+    if(isSmallScreen) {
+      panelTVert.current?.expand()
+      setPanelsDirection('vertical')
+    } else {
+      panelTVert.current?.collapse()
+      setPanelsDirection('horizontal')
+      panelLRef.current?.resize(50)
+    }
+  };
+  // Resets RHS & LHS to 50/50 when double clicking handle
+  const handleResizeDoubleClick = () => {
+    panelLRef.current?.resize(50);
+  };
+
   //Main content: loading, error, or normal
   let contentComponent;
 
@@ -728,8 +762,34 @@ export const Dashboard: NextPage = () => {
           </Grid>
           <Grid item xs={false} sm={5} md={5}></Grid>
         </Grid>
-        <Grid container component="main" wrap="wrap-reverse" spacing={2}>
-          <Grid item xs={12} sm={7} md={7}>
+        <PanelGroup className='pt-4' direction={panelsDirection}>
+          <Panel className='pb-1 min-h-fit' id='RHSVert' ref={panelTVert} collapsible={true}>
+            <Card>
+              <Carousel names={names}>{tabs}</Carousel>
+            </Card>
+          </Panel>
+          <PanelResizeHandle disabled />
+          <Panel className='pr-1 min-h-fit' id='LHS' ref={panelLRef} minSize={20} defaultSize={50}>
+            <SearchResultsTable
+              resultsLoading={results.state}
+              includedResults={includedResults}
+              grades={grades}
+              rmp={rmp}
+              compare={compare}
+              addToCompare={addToCompare}
+              removeFromCompare={removeFromCompare}
+            />
+          </Panel>
+          <PanelResizeHandle className='p-1 w-[2px] rounded-md opacity-75 transition ease-in-out bg-transparent hover:bg-blue-950 duration-75' onDoubleClick={handleResizeDoubleClick} />
+          <Panel className='pl-1' id='RHS' ref={panelRRef} minSize={40}>
+            <Card>
+              <Carousel names={names}>{tabs}</Carousel>
+            </Card>
+          </Panel>
+        </PanelGroup>
+      
+        {/* <Grid container component="main" wrap="wrap-reverse" spacing={2}>
+          <Grid item xs={12} sm={6} md={6}>
             <SearchResultsTable
               resultsLoading={results.state}
               includedResults={includedResults}
@@ -740,12 +800,12 @@ export const Dashboard: NextPage = () => {
               removeFromCompare={removeFromCompare}
             />
           </Grid>
-          <Grid item xs={false} sm={5} md={5} className="w-full">
+          <Grid item xs={false} sm={6} md={6}>
             <Card>
               <Carousel names={names}>{tabs}</Carousel>
             </Card>
           </Grid>
-        </Grid>
+        </Grid> */}
       </>
     );
   }
