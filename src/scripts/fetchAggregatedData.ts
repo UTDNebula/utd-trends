54997ef4197792bee00b85b2d53ca613ee72cc3a
Tyler Hill <gotyhill@gmail.com>
2024-11-04 13:00:53 -0600

    Lint fix

--- src/scripts/fetchAggregatedData.ts
+++ src/scripts/fetchAggregatedData.ts
@@ -11,21 +11,20 @@ config({ path: envPath });
 const API_KEY = process.env.REACT_APP_NEBULA_API_KEY;
 if (typeof API_KEY !== 'string') {
   console.error('API key is undefined');
-  return;
+} else {
+  const headers = {
+    'x-api-key': API_KEY,
+    Accept: 'application/json',
+  };
+
+  fetch('https://api.utdnebula.com/autocomplete/dag', {
+    method: 'GET',
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      writeFileSync('src/data/aggregated_data.json', JSON.stringify(data));
+
+      console.log('Aggregated data fetched.');
+    });
 }
-
-const headers = {
-  'x-api-key': API_KEY,
-  Accept: 'application/json',
-};
-
-fetch('https://api.utdnebula.com/autocomplete/dag', {
-  method: 'GET',
-  headers: headers,
-})
-  .then((response) => response.json())
-  .then((data) => {
-    writeFileSync('src/data/aggregated_data.json', JSON.stringify(data));
-
-    console.log('Aggregated data fetched.');
-  });
