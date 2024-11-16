3be4657dac9a7b3cc3dcabf7f0615b244fe861ea
Tyler Hill <gotyhill@gmail.com>
2024-11-15 20:28:07 -0600

    Revert "indicate that no tags are found if a professor with an rmp has no tags"
    This reverts commit 65e41ce07a711fb77df4572500f6faeecb41ff5a.

--- src/components/search/SearchResultsTable/searchResultsTable.tsx
+++ src/components/search/SearchResultsTable/searchResultsTable.tsx
@@ -172,16 +172,14 @@ function Row({
         <TableCell component="th" scope="row" className="w-full border-b-0">
           <Tooltip
             title={
-              (rmp !== undefined &&
-                rmp.state === 'done' &&
-                rmp.data.teacherRatingTags.length > 0 &&
-                'Tags: ' +
-                  rmp.data.teacherRatingTags
-                    .sort((a, b) => b.tagCount - a.tagCount)
-                    .slice(0, 3)
-                    .map((tag) => tag.tagName)
-                    .join(', ')) ||
-              (rmp !== undefined && rmp.state === 'done' && 'No Tags found')
+              rmp !== undefined &&
+              rmp.state === 'done' &&
+              'Tags: ' +
+                rmp.data.teacherRatingTags
+                  .sort((a, b) => b.tagCount - a.tagCount)
+                  .slice(0, 3)
+                  .map((tag) => tag.tagName)
+                  .join(', ')
             }
             placement="top"
           >
