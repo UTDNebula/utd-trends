0bdbf6c5ea1d2eb238e2fad22913aa28cee18ee3
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2024-11-20 10:02:56 -0600

    Try again to enable dynamic page titles alongside SSR

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -251,14 +251,11 @@ export async function getServerSideProps(
       pageTitle += term + ', ';
     });
   pageTitle = pageTitle.slice(0, -2) + (pageTitle.length > 0 ? ' - ' : '');
+  document.title = pageTitle;
   return { props: { pageTitle: pageTitle } };
 }
 
-export const Dashboard: NextPage<{ pageTitle: string }> = ({
-  pageTitle,
-}: {
-  pageTitle: string;
-}): React.ReactNode => {
+export const Dashboard: NextPage = () => {
   const router = useRouter();
 
   //Searches seperated into courses and professors to create combos
@@ -817,7 +814,7 @@ export const Dashboard: NextPage<{ pageTitle: string }> = ({
     );
   }
 
-  pageTitle = '';
+  let pageTitle = '';
   courses.map((term) => {
     pageTitle += searchQueryLabel(term) + ', ';
   });
