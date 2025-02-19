f444d81168b3a42247c7cd58f06c5dd639f47353
Govind <ivygovind@gmail.com>
2025-02-19 02:51:28 -0600

    added 'New Professor!' chip to all profs with blank recent sems

--- src/components/search/SearchResultsTable/searchResultsTable.tsx
+++ src/components/search/SearchResultsTable/searchResultsTable.tsx
@@ -253,7 +253,9 @@ function Row({
               </Typography>
             </Tooltip>
             {((typeof grades === 'undefined' || grades.state === 'error') && (
-              <></>
+              <Typography className="text-xs text-black text-center rounded-full px-1 py-1 ml-1 w-24 block bg-gray-100">
+                New Professor!
+              </Typography>
             )) ||
               (grades.state === 'loading' && (
                 <Skeleton
@@ -272,7 +274,7 @@ function Row({
                   placement="top"
                 >
                   <Typography className="text-xs text-black text-center rounded-full px-1 py-1 ml-1 w-8 block bg-gray-100">
-                    {most_recent_semester}
+                     {most_recent_semester}
                   </Typography>
                 </Tooltip>
               )) ||
