774b0b41d06df37ddf1c39e8f07e6817b4b4f8e0
Tyler Hill <gotyhill@gmail.com>
2025-02-20 19:02:14 -0600

    clean up line graph

--- src/components/common/SingleGradesInfo/singleGradesInfo.tsx
+++ src/components/common/SingleGradesInfo/singleGradesInfo.tsx
@@ -1,8 +1,9 @@
-import { Skeleton, ToggleButton, ToggleButtonGroup } from '@mui/material';
-import React, { useState } from 'react';
+import { Skeleton } from '@mui/material';
+import React from 'react';
 
 import BarGraph from '@/components/graph/BarGraph/barGraph';
 import LineGraph from '@/components/graph/LineGraph/lineGraph';
+import GraphToggle from '@/components/navigation/GraphToggle/graphToggle';
 import {
   type SearchQuery,
   searchQueryLabel,
@@ -16,25 +17,6 @@ function convertNumbersToPercents(distribution: GradesType): number[] {
   );
 }
 
-function getRecentSemesters() {
-  const recentSemesters: string[] = [];
-  const today = new Date();
-  const mm = today.getMonth() + 1;
-  let yyyy = today.getFullYear();
-  let season = mm <= 5 ? 'S' : 'F';
-
-  for (let i = 6; i >= 1; i--) {
-    if (season === 'S') {
-      yyyy -= 1;
-      season = 'F';
-    } else {
-      season = 'S';
-    }
-    recentSemesters.push(yyyy.toString().substring(2) + season);
-  }
-  return recentSemesters;
-}
-
 type Props = {
   title?: string;
   course: SearchQuery;
@@ -42,21 +24,14 @@ type Props = {
 };
 
 function SingleGradesInfo({ title, course, grades }: Props) {
-  const [chartType, setChartType] = useState<'line' | 'bar'>('line');
+  if (typeof grades === 'undefined' || grades.state === 'error') {
+    return null;
+  }
 
-  const handleChartToggle = (
-    event: React.MouseEvent<HTMLElement>,
-    newChartType: 'line' | 'bar',
-  ) => {
-    if (newChartType !== null) {
-      setChartType(newChartType);
-    }
-  };
-  if (typeof grades === 'undefined' || grades.state === 'error') return null;
   if (grades.state === 'loading') {
     return (
       <div className="p-2">
-        <Skeleton variant="rounded" className="w-full h-60 m-2" />
+        <GraphToggle state="loading" />
         <div className="flex flex-wrap justify-around">
           <p>
             Grades: <Skeleton className="inline-block w-[5ch]" />
@@ -68,99 +43,67 @@ function SingleGradesInfo({ title, course, grades }: Props) {
       </div>
     );
   }
+
   const percents = convertNumbersToPercents(grades.data);
-  const recentSemesters = getRecentSemesters();
-  const gpaValues = [
-    4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, 0.67, 0,
-  ];
 
   return (
-    <div>
-      {/* Toggle Button to switch chart type */}
-      <div className="flex justify-end mb-4 pr-2">
-        <ToggleButtonGroup
-          value={chartType}
-          exclusive
-          onChange={handleChartToggle}
-          size="small"
-          orientation="vertical"
-          aria-label="chart type"
-        >
-          <ToggleButton value="line" aria-label="line chart">
-            Line
-          </ToggleButton>
-          <ToggleButton value="bar" aria-label="bar chart">
-            Bar
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </div>
-      <div className="p-2">
-        {chartType === 'bar' && (
-          <>
-            <div className="h-64">
-              <BarGraph
-                title={title ?? '# of Students'}
-                xaxisLabels={[
-                  'A+',
-                  'A',
-                  'A-',
-                  'B+',
-                  'B',
-                  'B-',
-                  'C+',
-                  'C',
-                  'C-',
-                  'D+',
-                  'D',
-                  'D-',
-                  'F',
-                  'W',
-                ]}
-                yaxisFormatter={(value: number) =>
-                  Number(value).toFixed(0).toLocaleString()
-                }
-                tooltipFormatter={(
-                  value: number,
-                  { dataPointIndex }: { dataPointIndex: number },
-                ) =>
-                  Number(value).toFixed(0).toLocaleString() +
-                  ' (' +
-                  percents[dataPointIndex].toFixed(2) +
-                  '%)'
-                }
-                series={[
-                  {
-                    name: searchQueryLabel(course),
-                    data: grades.data.grade_distribution,
-                  },
-                ]}
-              />
-            </div>
-            <div className="flex flex-wrap justify-around">
-              <p>
-                Grades: <b>{grades.data.total.toLocaleString()}</b>
-              </p>
-              <p>
-                GPA:{' '}
-                <b>
-                  {grades.data.gpa === -1 ? 'None' : grades.data.gpa.toFixed(3)}
-                </b>
-              </p>
-            </div>
-          </>
-        )}
-
-        {chartType === 'line' && (
-          <div className="h-64">
-            <LineGraph
-              chartTitle="GPA Trend"
-              xAxisLabels={recentSemesters}
-              yAxisFormatter={(value: number) => value.toFixed(2)}
-              tooltipFormatter={(value: number) => value.toFixed(3)}
-              series={[{ name: searchQueryLabel(course), data: gpaValues }]}
-            />
-          </div>
-        )}
+    <div className="p-2">
+      <GraphToggle
+        state="done"
+        bar={
+          <BarGraph
+            title={title ?? '# of Students'}
+            xaxisLabels={[
+              'A+',
+              'A',
+              'A-',
+              'B+',
+              'B',
+              'B-',
+              'C+',
+              'C',
+              'C-',
+              'D+',
+              'D',
+              'D-',
+              'F',
+              'W',
+            ]}
+            yaxisFormatter={(value: number) =>
+              Number(value).toFixed(0).toLocaleString()
+            }
+            tooltipFormatter={(
+              value: number,
+              { dataPointIndex }: { dataPointIndex: number },
+            ) =>
+              Number(value).toFixed(0).toLocaleString() +
+              ' (' +
+              percents[dataPointIndex].toFixed(2) +
+              '%)'
+            }
+            series={[
+              {
+                name: searchQueryLabel(course),
+                data: grades.data.grade_distribution,
+              },
+            ]}
+          />
+        }
+        line={
+          <LineGraph
+            title="GPA Trend"
+            series={[{ name: searchQueryLabel(course), data: grades.data.grades }]}
+          />
+        }
+      />
+      <div className="flex flex-wrap justify-around">
+        <p>
+          Grades: <b>{grades.data.total.toLocaleString()}</b>
+        </p>
+        <p>
+          GPA:{' '}
+          <b>{grades.data.gpa === -1 ? 'None' : grades.data.gpa.toFixed(3)}</b>
+        </p>
       </div>
     </div>
   );
--- src/components/graph/LineGraph/lineGraph.tsx
+++ src/components/graph/LineGraph/lineGraph.tsx
@@ -1,157 +1,220 @@
-import { Skeleton } from '@mui/material';
+import { Card, Fade, Modal, useMediaQuery } from '@mui/material';
 import type { ApexOptions } from 'apexcharts';
 import dynamic from 'next/dynamic';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import resolveConfig from 'tailwindcss/resolveConfig';
 
-import type { GenericFetchedData } from '@/pages/dashboard/index';
+import tailwindConfig from '@/../tailwind.config.js';
+import { FullscreenCloseIcon } from '@/components/icons/FullscreenCloseIcon/fullscreenCloseIcon';
+import { FullscreenOpenIcon } from '@/components/icons/FullscreenOpenIcon/fullscreenOpenIcon';
+import { compareColors } from '@/modules/colors/colors';
+import type { GradesData } from '@/pages/api/grades';
+
+const fullTailwindConfig = resolveConfig(tailwindConfig);
+
+function sortSemesters(a: string, b: string) {
+  let aNum = parseInt(a);
+  if (a.includes('S')) {
+    aNum += 0.1;
+  } else if (a.includes('U')) {
+    aNum += 0.2;
+  } else {
+    aNum += 0.3;
+  }
+  let bNum = parseInt(b);
+  if (b.includes('S')) {
+    bNum += 0.1;
+  } else if (b.includes('U')) {
+    bNum += 0.2;
+  } else {
+    bNum += 0.3;
+  }
+  return aNum - bNum;
+}
+
+function getSemesterGPAs(
+  data: {
+    name: string;
+    data: GradesData;
+  },
+  allSemesters: string[],
+) {
+  const semesters = data.data
+    //remove summer
+    .filter((semester) => !semester._id.includes('U'))
+    .toSorted((a, b) => sortSemesters(a._id, b._id))
+    //get gpa and place in allSemesters
+    .map((semester) => {
+      const total: number = semester.grade_distribution.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0,
+      );
+
+      const GPALookup = [
+        4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, 0.67, 0,
+      ];
+      let gpa = 0;
+      if (total !== 0) {
+        gpa =
+          GPALookup.reduce(
+            (accumulator, currentValue, index) =>
+              accumulator + currentValue * semester.grade_distribution[index],
+            0,
+          ) /
+          (total -
+            semester.grade_distribution[
+              semester.grade_distribution.length - 1
+            ]);
+      }
+      return {
+        x: allSemesters.indexOf(semester._id) + 1,
+        y: gpa,
+      };
+    });
+  return {
+    name: data.name,
+    data: semesters,
+  };
+}
 
 // Dynamically import react-apexcharts with SSR disabled.
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-export type GPATrendType = {
-  season: string[];
-  years: string[];
-  gpa: number[];
-};
-
-type SeriesType = {
-  name: string;
-  data: number[];
-};
-
 type Props = {
-  gpaTrend?: GenericFetchedData<GPATrendType>;
-  chartTitle?: string;
+  title: string;
   xAxisLabels?: string[];
-  yAxisFormatter?: (value: number) => string;
-  tooltipFormatter?: (value: number) => string;
-  series?: SeriesType[];
-  height?: number;
+  series: {
+    name: string;
+    data: GradesData;
+  }[];
+  includedColors?: boolean[];
 };
 
-const LineGraph = ({
-  gpaTrend,
-  chartTitle,
-  xAxisLabels,
-  yAxisFormatter,
-  tooltipFormatter,
-  series,
-  height = 250,
-}: Props): JSX.Element => {
-  const [chartData, setChartData] = useState<{
-    options: ApexOptions;
-    series: SeriesType[];
-  }>({
-    options: {
-      chart: {
-        id: 'gpa-trend',
-        animations: {
-          enabled: true,
-          speed: 800,
-        },
-        toolbar: {
-          show: false,
-        },
-        zoom: {
-          enabled: true,
+const LineGraph = (props: Props): JSX.Element => {
+  const [fullScreenOpen, setFullScreenOpen] = useState<boolean>(false);
+
+  const icon =
+    '<div class="apexcharts-menu-icon">' +
+    (fullScreenOpen ? FullscreenCloseIcon : FullscreenOpenIcon) +
+    '</div>';
+
+  const noDataText = 'Please select a class to add';
+  // get all semesters from all series
+  const combinedSemesters = Array.from(
+    new Set(
+      props.series.flatMap((single) =>
+        single.data.map((semester) => semester._id),
+      ),
+    ),
+  )
+    .filter((semester) => !semester.includes('U'))
+    .toSorted(sortSemesters);
+  // fill in semester inbetween
+  const minSemester = combinedSemesters[0];
+  const maxSemester = combinedSemesters[combinedSemesters.length - 1];
+  const allSemesters: string[] = [];
+  for (let i = parseInt(minSemester); i <= parseInt(maxSemester); i++) {
+    if (i !== parseInt(minSemester) || minSemester.includes('S')) {
+      allSemesters.push(i + 'S');
+    }
+    if (i !== parseInt(maxSemester) || minSemester.includes('F')) {
+      allSemesters.push(i + 'F');
+    }
+  }
+  // format series with gpas and semester places
+  const series = props.series.map((single) => getSemesterGPAs(single, allSemesters));
+
+  const options: ApexOptions = {
+    chart: {
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        tools: {
+          customIcons: [
+            {
+              icon: icon,
+              index: 0,
+              title: 'Fullscreen',
+              class: 'custom-icon',
+              click: () => setFullScreenOpen(!fullScreenOpen),
+            },
+          ],
         },
       },
-      xaxis: {
-        categories: xAxisLabels || [],
-        labels: {
-          rotate: -45,
-        },
-        title: {
-          text: 'Semester',
-          style: {
-            fontSize: '12px',
-            fontWeight: '600',
-          },
-        },
-      },
-      yaxis: {
-        min: 1,
-        max: 4,
-        title: {
-          text: 'GPA',
-          style: {
-            fontSize: '12px',
-            fontWeight: '600',
-          },
-        },
-        labels: {
-          formatter: (value: number) =>
-            yAxisFormatter ? yAxisFormatter(value) : value.toFixed(2),
-        },
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      markers: {
-        size: 5,
-      },
-      tooltip: {
-        y: {
-          formatter: (value: number) =>
-            tooltipFormatter ? tooltipFormatter(value) : value.toFixed(3),
-        },
-        theme: 'dark',
-      },
-      legend: {
-        position: 'top',
-        horizontalAlign: 'right',
-        floating: true,
-        offsetY: -25,
-        offsetX: -5,
+      background: 'transparent',
+      animations: {
+        enabled: !fullScreenOpen,
       },
     },
-    series: series || [
-      {
-        name: chartTitle || '',
-        data: [],
+    legend: {
+      show: series.length !== 1,
+    },
+    xaxis: {
+      categories: allSemesters,
+    },
+    yaxis: {
+      min: 1,
+      max: 4,
+      labels: {
+        formatter: (value: number) => value.toFixed(2),
       },
-    ],
-  });
+    },
+    colors:
+      series.length === 1
+        ? // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+          [(fullTailwindConfig.theme.colors as any).royal as string]
+        : compareColors.filter(
+            (searchQuery, i) => props.includedColors?.[i] ?? 1,
+          ),
+    stroke: {
+      curve: 'straight',
+    },
+    title: {
+      text: props.title,
+      align: 'left',
+      style: {
+        fontFamily: 'inherit',
+      },
+    },
+    noData: {
+      text: noDataText,
+      align: 'center',
+      verticalAlign: 'middle',
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+        fontSize: '14px',
+        fontFamily: 'inherit',
+      },
+    },
+    theme: {
+      mode: useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light',
+    },
+    markers: {
+      size: 4,
+    },
+  };
 
-  useEffect(() => {
-    if (gpaTrend && gpaTrend.state === 'done') {
-      setChartData((prev) => ({
-        ...prev,
-        options: {
-          ...prev.options,
-          xaxis: {
-            ...prev.options.xaxis,
-            categories: gpaTrend.data.season,
-          },
-        },
-        series: [
-          {
-            name: chartTitle || '',
-            data: gpaTrend.data.gpa,
-          },
-        ],
-      }));
-    } else if (series) {
-      setChartData((prev) => ({
-        ...prev,
-        series: series,
-      }));
-    }
-  }, [gpaTrend, series, chartTitle]);
-
-  if (gpaTrend && gpaTrend.state === 'loading') {
-    return <Skeleton variant="rounded" height={height} />;
-  }
+  const graph = (
+    <div className="h-full">
+      <Chart options={options} series={series} type="line" height={'100%'} />
+    </div>
+  );
 
   return (
-    <div>
-      <Chart
-        options={chartData.options}
-        series={chartData.series}
-        type="line"
-        height={height}
-      />
-    </div>
+    <>
+      {graph}
+      <Modal
+        open={fullScreenOpen}
+        onClose={() => setFullScreenOpen(false)}
+        className="flex justify-stretch align-stretch"
+      >
+        <Fade in={fullScreenOpen}>
+          <Card className="p-4 m-12 flex-auto">{graph}</Card>
+        </Fade>
+      </Modal>
+    </>
   );
 };
 
