5d418e702934f9a7b9460b314eee7e31d310a6cd
NikhilMarisetty23 <46114287+sainikhil1611@users.noreply.github.com>
2025-02-28 10:23:56 -0600

    Prettier formatting

--- src/components/common/Rating/rating.tsx
+++ src/components/common/Rating/rating.tsx
@@ -2,11 +2,11 @@ import { Rating as _Rating, styled } from '@mui/material';
 
 const Rating = styled(_Rating)(({ theme }) => ({
   '& .MuiRating-iconFilled': {
-    color: theme.palette.mode === 'light' 
-      ? theme.palette.primary.main // Use primary.main in light mode
-      : theme.palette.primary.light, // Use primary.light in dark mode
+    color:
+      theme.palette.mode === 'light'
+        ? theme.palette.primary.main // Use primary.main in light mode
+        : theme.palette.primary.light, // Use primary.light in dark mode
   },
 }));
-
 
 export default Rating;
