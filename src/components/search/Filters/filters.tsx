3b041d8589517bff43cbfad871da8e382e4d06fb
George-Zhang5246 <70183314+George-Zhang5246@users.noreply.github.com>
2025-03-03 19:11:39 -0600

    Update filters.tsx

--- src/components/search/Filters/filters.tsx
+++ src/components/search/Filters/filters.tsx
@@ -55,7 +55,7 @@ const Filters = ({
     }
   }, [router.isReady, router.query.minGPA, router.query.minRating]); // useEffect is called on query update (so on back navigation, the filters selected are set based on the url)
 
-  /*useEffect(() => {
+  useEffect(() => {
     if (chosenSessions.length === academicSessions.length) {
       setSemesters(() => chosenSessions.concat(['select-all']));
     } else if (
@@ -66,7 +66,7 @@ const Filters = ({
     } else {
       setSemesters(() => chosenSessions);
     }
-  }, [chosenSessions]);*/
+  }, [chosenSessions]);
   
   function getRecentSemesters() {
     let recentSemesters: string[] = [];
