188668183a32e6460232b41ce9aef9fb08697433
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-03-09 19:46:25 -0500

    Make the height of a PlannerSection dynamic

--- src/components/planner/PlannerSchedule/PlannerSection.tsx
+++ src/components/planner/PlannerSchedule/PlannerSection.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 import { DAYS, numDays, type SectionData, START_HOUR } from './plannerSchedule';
 
-type SectionComponentProps = {
+type PlannerSectionComponentProps = {
   selectedSection: SectionData;
   key: number;
 };
 
-const Section = (props: SectionComponentProps) => {
+const PlannerSection = (props: PlannerSectionComponentProps) => {
   //const SectionComponent = () => {
   const [meetings, setMeetings] = useState<string[][]>([]);
   useEffect(() => {
@@ -23,16 +23,30 @@ const Section = (props: SectionComponentProps) => {
           startRow += 12;
         }
 
-        /**  TODO: FIX THIS OBVIOUSLY **/
-        const lengthPercentHour = '125';
+        /**  TODO: what if no start/endtime **/
+        const s_h = Number(meeting.start_time.split(':')[0]) + (meeting.start_time.includes('pm') ? 12 : 0)
+        const s_m = Number(meeting.start_time.split(':')[1].slice(0,2))
+        let e_h = Number(meeting.end_time.split(':')[0]) + (meeting.end_time.includes('pm') ? 12 : 0)
+        const e_m = Number(meeting.end_time.split(':')[1].slice(0,2))
 
-        const offset =
-          Number(splitStartTime[1].slice(0, splitStartTime[1].length - 2)) / 60;
+        let d_h = 0;
+        let d_m = 0;
+        if (e_m < s_m) { // lmao i'm doing elementary carry subtraction
+          e_h--;
+          d_m = (e_m + 60) - s_m
+        }
+        else
+          d_m = e_m - s_m;
+        d_h = e_h - s_h;
+          
+        const lengthPercentHour = (d_h*60 + d_m) * 125/90;
+
+        const offset = Number(splitStartTime[1].slice(0, splitStartTime[1].length - 2)) / 60;
         const offsetTotalPercent = offset * 100;
         tempMeetings.push([
           col.toString(),
           startRow.toString(),
-          lengthPercentHour,
+          lengthPercentHour.toString(),
           offsetTotalPercent.toString(),
         ]);
       }
@@ -54,9 +68,9 @@ const Section = (props: SectionComponentProps) => {
       }
       className={`col-start-[var(--start-col)] col-span-1 row-start-[var(--start-row)] row-span-1 relative top-[var(--offset)] h-[var(--height)] overflow-visible rounded-lg bg-cornflower-500`}
     >
-      Section
+      <div>{}</div>
     </div>
   ));
 };
 
-export default Section;
+export default PlannerSection;
