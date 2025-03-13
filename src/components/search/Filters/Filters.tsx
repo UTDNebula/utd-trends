5ac36637d56f9623a280e493582d732849b8a497
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2025-03-12 19:45:01 -0500

    added xUx for the summer classes

--- src/components/search/Filters/Filters.tsx
+++ src/components/search/Filters/Filters.tsx
@@ -138,6 +138,7 @@ const Filters = ({
       '7xx': 'Exam section',
       HNx: 'Honors-only',
       HON: 'Honors-only',
+      'xUx': 'Summer Class'
     };
 
     return courseTypeMap[id] || id; // Default to ID if no mapping exists
