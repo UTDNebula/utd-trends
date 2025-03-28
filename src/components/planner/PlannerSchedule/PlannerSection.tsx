57aaa02faa2075fcc157bc56cd9b370ba76f033c
SashaankG <sashaank101@gmail.com>
2025-03-28 15:48:59 -0500

    formatted

--- src/components/planner/PlannerSchedule/PlannerSection.tsx
+++ src/components/planner/PlannerSchedule/PlannerSection.tsx
@@ -54,15 +54,15 @@ const PlannerSection = (props: PlannerSectionComponentProps) => {
         const offset =
           Number(splitStartTime[1].slice(0, splitStartTime[1].length - 2)) / 60;
         const offsetTotalPercent = offset * 100;
-        
-        const dayKey = meeting_day === "Thursday" ? "Z" : meeting_day.charAt(0);
-        
+
+        const dayKey = meeting_day === 'Thursday' ? 'Z' : meeting_day.charAt(0);
+
         tempMeetings.push([
           col.toString(),
           startRow.toString(),
           lengthPercentHour.toString(),
           offsetTotalPercent.toString(),
-          dayKey
+          dayKey,
         ]);
       }
     }
@@ -73,39 +73,40 @@ const PlannerSection = (props: PlannerSectionComponentProps) => {
   //added color map colors are pretty ugly so change them based on figma color codes
   //dont know what to do for firday combo classes or sat
   const colorMap: { [key: string]: string } = {
-    M: "bg-purple-400",
-    W: "bg-purple-400",
-    T: "bg-purple-800",
-    Z: "bg-purple-800",
+    M: 'bg-purple-400',
+    W: 'bg-purple-400',
+    T: 'bg-purple-800',
+    Z: 'bg-purple-800',
   };
-  
+
   return meetings.map((x: string[], i: number) => (
     <div
       key={props.selectedSection._id + i}
       style={
         {
-          "--start-col": x[0],
-          "--start-row": x[1],
-          "--offset": x[3] + "%",
-          "--height": x[2] + "%",
+          '--start-col': x[0],
+          '--start-row': x[1],
+          '--offset': x[3] + '%',
+          '--height': x[2] + '%',
         } as React.CSSProperties
       }
       className={`col-start-[var(--start-col)] col-span-1 
         row-start-[var(--start-row)] row-span-1 relative 
         top-[var(--offset)] h-[var(--height)] overflow-visible 
-        rounded-lg ${colorMap[x[4]] || "bg-purple-100"} 
+        rounded-lg ${colorMap[x[4]] || 'bg-purple-100'} 
         m-1 leading-relaxed shadow-md text-white`}
     >
-
-        <div className="text-lg font-lato text-center">
-          {props.course.prefix} {props.course.number}.{props.selectedSection.section_number}
-        </div>
-        <div className="text-xs text-center">
-          {props.course.profFirst} {props.course.profLast}
-        </div>
-        <div className="text-xs text-center">
-          {props.selectedSection.meetings[0]?.location?.building} {props.selectedSection.meetings[0]?.location?.room}
-        </div>
+      <div className="text-lg font-lato text-center">
+        {props.course.prefix} {props.course.number}.
+        {props.selectedSection.section_number}
+      </div>
+      <div className="text-xs text-center">
+        {props.course.profFirst} {props.course.profLast}
+      </div>
+      <div className="text-xs text-center">
+        {props.selectedSection.meetings[0]?.location?.building}{' '}
+        {props.selectedSection.meetings[0]?.location?.room}
+      </div>
     </div>
   ));
 };
