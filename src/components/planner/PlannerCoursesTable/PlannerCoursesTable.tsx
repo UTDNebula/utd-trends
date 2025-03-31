4a3281de898403dd971a5d940be3575ec06b54c5
Tyler Hill <gotyhill@gmail.com>
2025-03-31 12:41:25 -0500

    Fix planner empty

--- src/components/planner/PlannerCoursesTable/PlannerCoursesTable.tsx
+++ src/components/planner/PlannerCoursesTable/PlannerCoursesTable.tsx
@@ -18,7 +18,7 @@ import { type SectionsType } from '@/modules/SectionsType/SectionsType';
 import type { RMPInterface } from '@/pages/api/ratemyprofessorScraper';
 
 type PlannerCoursesTableProps = {
-  courses?: SearchQueryMultiSection[];
+  courses: SearchQueryMultiSection[];
   addToPlanner: (value: SearchQuery) => void;
   removeFromPlanner: (value: SearchQuery) => void;
   setPlannerSection: (searchQuery: SearchQuery, section: string) => boolean;
@@ -32,49 +32,45 @@ type PlannerCoursesTableProps = {
 const PlannerCoursesTable = (props: PlannerCoursesTableProps) => {
   return (
     <>
-      <Typography className="leading-tight text-3xl font-bold p-4">
+      <Typography variant="h2" className="leading-tight text-3xl font-bold p-4">
         My Planner
       </Typography>
       <div className="flex flex-col gap-4">
-        {props.courses
-          ? props.courses.map((course, index) => {
-              const sectionData =
-                props.sections[searchQueryLabel(removeSection(course))];
+        {props.courses.map((course, index) => {
+          const sectionData =
+            props.sections[searchQueryLabel(removeSection(course))];
 
-              if (typeof sectionData !== 'undefined') {
-                if (sectionData.state === 'loading') {
-                  return <LoadingRow key={index} />;
-                }
+          if (typeof sectionData !== 'undefined') {
+            if (sectionData.state === 'loading') {
+              return <LoadingRow key={index} />;
+            }
+          }
+          return (
+            <PlannerCard
+              key={index}
+              query={course}
+              sections={
+                typeof sectionData === 'undefined' ||
+                sectionData.state === 'error'
+                  ? undefined
+                  : sectionData.data.latest.filter((section) => {
+                      if (
+                        typeof course.profFirst === 'undefined' &&
+                        typeof course.profLast === 'undefined'
+                      ) {
+                        return sectionCanOverlap(section.section_number);
+                      } else return true;
+                    })
               }
-              return (
-                <PlannerCard
-                  key={index}
-                  query={course}
-                  sections={
-                    typeof sectionData === 'undefined' ||
-                    sectionData.state === 'error'
-                      ? undefined
-                      : sectionData.data.latest.filter((section) => {
-                          if (
-                            typeof course.profFirst === 'undefined' &&
-                            typeof course.profLast === 'undefined'
-                          ) {
-                            return sectionCanOverlap(section.section_number);
-                          } else return true;
-                        })
-                  }
-                  setPlannerSection={props.setPlannerSection}
-                  grades={props.grades[searchQueryLabel(removeSection(course))]}
-                  rmp={props.rmp[searchQueryLabel(convertToProfOnly(course))]}
-                  removeFromPlanner={() => {
-                    props.removeFromPlanner(course);
-                  }}
-                />
-              );
-            })
-          : Array(5)
-              .fill(0)
-              .map((_, index) => <LoadingRow key={index} />)}
+              setPlannerSection={props.setPlannerSection}
+              grades={props.grades[searchQueryLabel(removeSection(course))]}
+              rmp={props.rmp[searchQueryLabel(convertToProfOnly(course))]}
+              removeFromPlanner={() => {
+                props.removeFromPlanner(course);
+              }}
+            />
+          );
+        })}
       </div>
     </>
   );
---
+++ src/components/planner/PlannerEmpty/PlannerEmpty.tsx
@@ -0,0 +1,20 @@
+import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function MyPlannerEmpty() {
+  return (
+    <>
+      <Typography variant="h2" className="leading-tight text-3xl font-bold p-4">
+        My Planner
+      </Typography>
+      <Typography variant="h3" className="text-2xl p-4">
+        {'Add a course on the search results page with the '}
+        <BookOutlinedIcon className="text-2xl" />
+        {' icon.'}
+      </Typography>
+    </>
+  );
+}
+
+export default MyPlannerEmpty;
