e9c2bbc2683ac8bf18552a51a1c55fd2cd7139fc
Tyler Hill <gotyhill@gmail.com>
2025-03-31 22:59:55 -0500

    finish merge

--- src/components/navigation/TopMenu/TopMenu.tsx
+++ src/components/navigation/TopMenu/TopMenu.tsx
@@ -1,12 +1,15 @@
-import { Share } from '@mui/icons-material';
 import BookIcon from '@mui/icons-material/Book';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import ShareIcon from '@mui/icons-material/Share';
 import { Button, IconButton, Snackbar, Tooltip } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Background from '@/../public/background.png';
+import WhatsNew from '@/components/common/WhatsNew/WhatsNew';
+import Tutorial from '@/components/dashboard/Tutorial/Tutorial';
 import SearchBar from '@/components/search/SearchBar/SearchBar';
 
 /**
@@ -55,6 +58,25 @@ export function TopMenu(props: TopMenuProps) {
   function alertLink(url: string) {
     alert(url);
   }
+
+  const [openTutorial, setOpenTutorial] = useState(false);
+  const closeTutorial = useCallback(() => setOpenTutorial(false), []);
+  const [tutorialHint, setTutorialHint] = useState(false);
+  //Open if not already closed (based on localStorage)
+  useEffect(() => {
+    const previous = localStorage.getItem('tutorialHint');
+    let ask = previous === null;
+    if (previous !== null) {
+      const parsed = JSON.parse(previous);
+      if (parsed !== null && parsed.value !== 'opened') {
+        ask = true;
+      }
+    }
+    if (ask) {
+      setTutorialHint(true);
+    }
+  }, []);
+  const cacheIndex = 0; //Increment this to open the popup for all users on next deployment
 
   return (
     <>
@@ -112,25 +134,68 @@ export function TopMenu(props: TopMenuProps) {
             {props.isPlanner ? 'Search Results' : 'My Planner'}
           </Button>
         </Link>
-        <Tooltip title="Share link to search">
-          <IconButton
-            className="aspect-square"
-            size="medium"
-            onClick={() => {
-              let url = window.location.href;
-              if (
-                router.query &&
-                Object.keys(router.query).length === 0 &&
-                Object.getPrototypeOf(router.query) === Object.prototype
-              ) {
-                url = 'https://trends.utdnebula.com/';
+        <div className="flex gap-0 md:gap-4 ml-auto">
+          <div className="ml-auto">
+            <WhatsNew />
+          </div>
+          <div className="relative">
+            <div
+              className={
+                tutorialHint
+                  ? 'absolute w-11 h-11 rounded-full bg-royal dark:bg-cornflower-400 animate-ping'
+                  : 'hidden'
               }
-              shareLink(url);
-            }}
-          >
-            <Share className="text-3xl mr-1" />
-          </IconButton>
-        </Tooltip>
+            />
+            <div
+              className={
+                tutorialHint
+                  ? ' rounded-full bg-royal dark:bg-cornflower-400'
+                  : ''
+              }
+            >
+              <Tooltip title="Open Tutorial">
+                <IconButton
+                  className="aspect-square"
+                  size="medium"
+                  onClick={() => {
+                    setTutorialHint(false);
+                    localStorage.setItem(
+                      'tutorialHint',
+                      JSON.stringify({
+                        value: 'opened',
+                        cacheIndex: cacheIndex,
+                      }),
+                    );
+                    setOpenTutorial(true);
+                  }}
+                >
+                  <HelpOutlineIcon
+                    className={'text-3xl' + (tutorialHint ? ' text-white' : '')}
+                  />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </div>
+          <Tooltip title="Share Link to Search">
+            <IconButton
+              className="aspect-square"
+              size="medium"
+              onClick={() => {
+                let url = window.location.href;
+                if (
+                  router.query &&
+                  Object.keys(router.query).length === 0 &&
+                  Object.getPrototypeOf(router.query) === Object.prototype
+                ) {
+                  url = 'https://trends.utdnebula.com/';
+                }
+                shareLink(url);
+              }}
+            >
+              <ShareIcon className="text-3xl mr-0.5 -ml-0.5" />
+            </IconButton>
+          </Tooltip>
+        </div>
       </div>
       <Snackbar
         open={openCopied}
@@ -138,6 +203,7 @@ export function TopMenu(props: TopMenuProps) {
         onClose={() => setOpenCopied(false)}
         message="Copied!"
       />
+      <Tutorial open={openTutorial} close={closeTutorial} />
     </>
   );
 }
