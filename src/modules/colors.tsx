fb8e8673d99a0eee4cbe6b487658ab992f1e6c5c
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-04-07 02:11:54 -0500

    reorder plannerColors

--- src/modules/colors.tsx
+++ src/modules/colors.tsx
@@ -69,10 +69,10 @@ export const plannerColors = [
   { fill: '#FFA6C2', outline: '#FEC2D5', font: '#780909' },
   { fill: '#D4A6E2', outline: '#DEBDE8', font: '#590972' },
   { fill: '#93BDFF', outline: '#B1CFFF', font: '#0E397C' },
-  { fill: '#E9D0AC', outline: '#FFECD0', font: '#5D3804' },
+  { fill: '#F9C28A', outline: '#FFCDB7', font: '#611F00' },
   { fill: '#7BD6DD', outline: '#A2F4FA', font: '#034F55' },
+  { fill: '#E9D0AC', outline: '#FFECD0', font: '#5D3804' },
   { fill: '#89EDAF', outline: '#B0FBCD', font: '#335600' },
   { fill: '#D3C1FB', outline: '#EEE6FF', font: '#483080' },
-  { fill: '#F9C28A', outline: '#FFCDB7', font: '#611F00' },
   { fill: '#9FF9C9', outline: '#BFFFDD', font: '#005025' },
 ];
