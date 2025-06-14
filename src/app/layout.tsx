b7183c399375380ff364332e4ca89398e580ae59
Tyler Hill <gotyhill@gmail.com>
2025-06-13 21:01:46 -0700

    Switch to imports in Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import localFont from 'next/font/local';
 import React from 'react';
 
 import FeedbackPopup from '@/components/common/FeedbackPopup/FeedbackPopup';
@@ -14,72 +12,6 @@ import GitHubButton from '@/components/common/GitHubButton/GitHubButton';
 import theme from '@/modules/theme';
 
 import { SharedStateProvider } from './SharedStateProvider';
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
-const kallisto = localFont({
-  src: [
-    /*
-    {
-      path: '../fonts/Kallisto/Kallisto Thin.otf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Kallisto/Kallisto Thin Italic.otf',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: '../fonts/Kallisto/Kallisto Light.otf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Kallisto/Kallisto Light Italic.otf',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: '../fonts/Kallisto/Kallisto Medium.otf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../fonts/Kallisto/Kallisto Medium Italic.otf',
-      weight: '500',
-      style: 'italic',
-    },
-    */
-    {
-      path: '../fonts/Kallisto/Kallisto Bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-    /*
-    {
-      path: '../fonts/Kallisto/Kallisto Bold Italic.otf',
-      weight: '700',
-      style: 'italic',
-    },
-    */
-    {
-      path: '../fonts/Kallisto/Kallisto Heavy.otf',
-      weight: '900',
-      style: 'normal',
-    },
-    /*
-    {
-      path: '../fonts/Kallisto/Kallisto Heavy Italic.otf',
-      weight: '900',
-      style: 'italic',
-    },
-    */
-  ],
-  variable: '--font-kallisto',
-});
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://trends.utdnebula.com'),
@@ -130,7 +62,7 @@ export default function RootLayout({
         <GoogleAnalytics gaId="G-CC86XR1562" />
       )}
       <body
-        className={`bg-white dark:bg-black ${inter.variable} font-inter ${kallisto.variable} text-haiti dark:text-white`}
+        className="bg-white dark:bg-black font-inter text-haiti dark:text-white"
       >
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
