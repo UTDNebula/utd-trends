6881b8969dd1615b7f9dcb26411bda24a25b439f
Tyler Hill <gotyhill@gmail.com>
2025-07-22 21:28:06 -0700

    Revert remove widenClientFileUpload

--- next.config.js
+++ next.config.js
@@ -20,6 +20,9 @@ module.exports = withSentryConfig(module.exports, {
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
