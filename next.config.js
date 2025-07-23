079e112c1050bacd216a1bad84bc29d3b7c246e8
Tyler Hill <gotyhill@gmail.com>
2025-07-22 21:13:04 -0700

    Sentry only on prod

--- next.config.js
+++ next.config.js
@@ -20,9 +20,6 @@ module.exports = withSentryConfig(module.exports, {
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
@@ -37,4 +34,11 @@ module.exports = withSentryConfig(module.exports, {
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
+
+  sourcemaps: {
+    disable: process.env.VERCEL_ENV !== 'production',
+  },
+  release: {
+    create: process.env.VERCEL_ENV === 'production',
+  },
 });
