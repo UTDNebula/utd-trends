6ea2130f2f0836d1d3d31e3b63b74a4f487ed45f
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-10-19 00:49:24 -0500

    remove dark mode conditionals

--- src/app/planner/PlannerDownloadUI.tsx
+++ src/app/planner/PlannerDownloadUI.tsx
@@ -12,9 +12,9 @@ export default function PlannerDownloadUI({ downloadRef } : { downloadRef : Reac
     return (    
         <div className='bg-black p-4 absolute left-[-9999px] top-0' ref={downloadRef}>
             <div className='flex flex-row mb-4 justify-start'>
-                <NebulaLogo className="h-6 w-auto fill-haiti dark:fill-white mr-2 ml-2" />
-                UTD TRENDS
-                <h1 className='ml-auto mr-2'>FALL 2025</h1>
+                <NebulaLogo className="h-6 w-auto fill-white mr-2 ml-2" />
+                <span className='font-extrabold font-display text-white'>UTD TRENDS</span>
+                <h1 className='ml-auto mr-2 text-white'>FALL 2025</h1>
             </div>
             <PlannerSchedule />
         </div>
