ef169d072317a60ae9ffb02c1277006658cf142f
egsch <egschaumann@gmail.com>
2025-10-22 15:46:35 -0500

    fix cropped-off logo

--- src/app/planner/PlannerDownloadUI.tsx
+++ src/app/planner/PlannerDownloadUI.tsx
@@ -11,8 +11,8 @@ export default function PlannerDownloadUI({ downloadRef } : { downloadRef : Reac
   
     return (    
         <div className='bg-black p-4 absolute left-[-9999px] top-0' ref={downloadRef}>
-            <div className='flex flex-row mb-4 justify-start'>
-                <NebulaLogo className="h-6 w-auto fill-white mr-2 ml-2" />
+            <div className='flex flex-row mb-4 justify-start gap-2 pl-2'>
+                <NebulaLogo className="h-6 w-auto fill-white" />
                 <span className='font-extrabold font-display text-white'>UTD TRENDS</span>
                 <h1 className='ml-auto mr-2 text-white'>FALL 2025</h1>
             </div>
