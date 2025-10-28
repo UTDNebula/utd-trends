b4e319c17d643d8ac1de4f9a5c56704d9492f057
egsch <egschaumann@gmail.com>
2025-10-28 10:27:32 -0500

    Update filter counts to account for semester filter

--- src/components/search/Filters/Filters.tsx
+++ src/components/search/Filters/Filters.tsx
@@ -139,9 +139,20 @@ export default function Filters({
   const gradeCounts: Record<string, number> = {};
   const rmpCounts: Record<string, number> = {};
 
+  const semFilteredResults = searchResults.filter((result) => {
+    const availableThisSemester = result.sections.some(
+      (section) => section.academic_session.name === latestSemester,
+    );
+    return (
+      result.grades.length == 0 ||
+      result.grades.some((s) => chosenSemesters.includes(s._id)) ||
+      availableThisSemester
+    );
+  });
+
   minGPAs.forEach((gpaString) => {
     const gpaNum = parseFloat(gpaString);
-    gradeCounts[gpaString] = searchResults.filter((result) => {
+    gradeCounts[gpaString] = semFilteredResults.filter((result) => {
       if (result.type !== 'course') {
         if (
           typeof minRating === 'string' &&
@@ -152,7 +163,8 @@ export default function Filters({
       }
       const courseGrades = result.grades;
       return (
-        (courseGrades && calculateGrades(courseGrades, chosenSemesters).gpa >= gpaNum) ||
+        (courseGrades &&
+          calculateGrades(courseGrades, chosenSemesters).gpa >= gpaNum) ||
         courseGrades == undefined
       );
     }).length;
@@ -160,20 +172,19 @@ export default function Filters({
 
   minRatings.forEach((ratingString) => {
     const ratingNum = parseFloat(ratingString);
-    rmpCounts[ratingString] = searchResults.filter((result) => {
-      // don't count classes already filtered out by semester or grade filters
-      const inSearchResults = result.grades.some((s)=>chosenSemesters.includes(s._id) || s._id == latestSemester)
+    rmpCounts[ratingString] = semFilteredResults.filter((result) => {
+      // gpa filter
+      const calculated = calculateGrades(result.grades, chosenSemesters);
+      if (typeof minGPA === 'string' && calculated.gpa < parseFloat(minGPA))
+        return false;
       if (
-        !inSearchResults ||
-        (typeof minGPA === 'string' &&
-        calculateGrades(result.grades, chosenSemesters).gpa < parseFloat(minGPA))
+        typeof ratingNum === 'number' &&
+        result.type !== 'course' &&
+        result.RMP &&
+        result.RMP.avgRating < ratingNum
       )
         return false;
-      return (
-        result.type !== 'course' &&
-        ((result.RMP && result.RMP.avgRating >= ratingNum) ||
-          !result.RMP?.avgRating)
-      );
+      return true;
     }).length;
   });
 
