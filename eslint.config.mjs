42b7ef26c1c8d890b529ebf5e9494405adf43c6c
Tyler Hill <gotyhill@gmail.com>
2026-01-19 15:35:12 -0600

    Next 16, ESLint, MUI, Sentry

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-  }),
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'styleguide/build/**',
+    'styleguide.config.js'
+  ]),
+]);
 
 export default eslintConfig;
