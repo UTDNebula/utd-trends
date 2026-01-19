8ca5b5d73953e3f62afdc4d844337ecae55cf09c
Tyler Hill <gotyhill@gmail.com>
2026-01-19 14:37:16 -0600

    Remove last references to REACT_APP_NEBULA_API_KEY

--- src/app/api/planner/route.ts
+++ src/app/api/planner/route.ts
@@ -3,7 +3,7 @@ import { fetchLatestSemester } from '@/modules/fetchSections';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const API_KEY = process.env.REACT_APP_NEBULA_API_KEY;
+  const API_KEY = process.env.NEBULA_API_KEY;
   if (typeof API_KEY !== 'string') {
     return NextResponse.json(
       { message: 'error', data: 'API key is undefined' },
