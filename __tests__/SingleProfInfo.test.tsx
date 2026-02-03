2a65b1cb043a518ce3684c250b4b644a8b6666de
egsch <egschaumann@gmail.com>
2026-02-03 17:48:14 -0600

    test: update test mocks

--- __tests__/SingleProfInfo.test.tsx
+++ __tests__/SingleProfInfo.test.tsx
@@ -3,13 +3,14 @@
  */
 
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
 import SingleProfInfo from '@/components/common/SingleProfInfo/SingleProfInfo';
+import { render, screen } from '@testing-library/react';
 import { mockRMP, mockRmpNoRatings } from '../mocks/RMP.mocks.tsx';
+import { mockSearchQuery } from '../mocks/SearchQuery.mocks.tsx';
 
 describe('SingleProfInfo', () => {
   it('displays average rating', () => {
-    render(<SingleProfInfo rmp={mockRMP} />);
+    render(<SingleProfInfo rmp={mockRMP} searchQuery={mockSearchQuery} />);
 
     // displays average rating
     const rating = screen.getByText(mockRMP.avgRating.toFixed(1));
@@ -18,7 +19,9 @@ describe('SingleProfInfo', () => {
   });
 
   it('displays N/A given no ratings', () => {
-    render(<SingleProfInfo rmp={mockRmpNoRatings} />);
+    render(
+      <SingleProfInfo rmp={mockRmpNoRatings} searchQuery={mockSearchQuery} />,
+    );
 
     // displays average rating
     const rating = screen.getByText('N/A');
