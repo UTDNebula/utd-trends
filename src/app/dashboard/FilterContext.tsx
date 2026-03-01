9d044925cdccf418bf1392d91c1ccb5702a3e767
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-02-28 22:33:27 -0600

    move `compare=true` logic to the dashboard's `page.tsx` instead of `FilterContext.tsx`

--- src/app/dashboard/FilterContext.tsx
+++ src/app/dashboard/FilterContext.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import Compare from '@/components/compare/Compare/Compare';
-import Header from '@/components/navigation/Header/Header';
 import { getSemestersFromSearchResults } from '@/modules/semesters';
 import type { SearchResult } from '@/types/SearchQuery';
-import { Card } from '@mui/material';
-import { useSearchParams } from 'next/navigation';
 import {
   createContext,
   useMemo,
@@ -30,7 +26,6 @@ export default function FiltersProvider({
   children: ReactNode;
 }) {
   const { compare } = useSharedState();
-  const params = useSearchParams();
   const semesters = useMemo(() => {
     return getSemestersFromSearchResults(searchResults.concat(compare));
   }, [searchResults, compare]);
@@ -50,18 +45,7 @@ export default function FiltersProvider({
     <FiltersContext.Provider
       value={{ chosenSemesters, setChosenSemesters, semesters }}
     >
-      {params.get('compare') === 'true' ? (
-        <>
-          <Header isPlanner={true} />
-          <main className="p-4 overflow-y-auto">
-            <Card className="w-full p-4">
-              <Compare />
-            </Card>
-          </main>
-        </>
-      ) : (
-        children
-      )}
+      {children}
     </FiltersContext.Provider>
   );
 }
