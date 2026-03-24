d470d9d456864bc5a8c4d9bb75f4f5e67dc28c31
egsch <egschaumann@gmail.com>
2026-03-23 21:54:27 -0500

    Fix grade count for mean & median calculation

--- src/modules/fetchGrades.ts
+++ src/modules/fetchGrades.ts
@@ -50,21 +50,29 @@ export function calculateGrades(
   const GPALookup = [
     4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, 0.67, 0,
   ];
+
+  const totalLetterGrades =
+    total -
+    grade_distribution
+      .slice(GPALookup.length)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
   let mean_gpa = -1;
-  if (total !== 0) {
+
+  if (totalLetterGrades !== 0) {
     mean_gpa =
       GPALookup.reduce(
         (accumulator, currentValue, index) =>
           accumulator + currentValue * grade_distribution[index],
         0,
-      ) /
-      (total - grade_distribution[grade_distribution.length - 1]);
+      ) / totalLetterGrades;
+    // sum the rest of the array (values without GPAs)
   }
 
   let median_gpa = -1;
   let medianIndex = -1;
-  if (total != 0) {
-    let i = Math.floor(total / 2);
+  if (totalLetterGrades != 0) {
+    let i = Math.floor(totalLetterGrades / 2);
     while (i > 0) {
       medianIndex++;
       i -= grade_distribution[medianIndex];
