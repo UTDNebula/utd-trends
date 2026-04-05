2568771a03aad281a8d353eb4e18a6ba91fd6672
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-04-05 09:55:29 -0500

    add a container in `layout.tsx` to allow overscroll without overscrolling the entire `<body/>`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,32 +80,34 @@ export default async function RootLayout({
   );
 
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
         <GoogleAnalytics gaId="G-CC86XR1562" />
       )}
       <body
-        className={`bg-[rgb(246,246,246)] dark:bg-black ${inter.variable} font-main ${baiJamjuree.variable} text-haiti dark:text-white min-h-full flex flex-col`}
+        className={`bg-[rgb(246,246,246)] dark:bg-black ${inter.variable} font-main ${baiJamjuree.variable} text-haiti dark:text-white`}
       >
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <QueryProvider>
-              <SharedStateProvider
-                availableSemesters={availableSemesters}
-                defaultTeachingSemester={latestSemester}
-              >
-                <div className="flex-1 pb-[var(--mobile-nav-height)] md:pb-0">
-                  {children}
-                </div>
-                <Suspense fallback={null}>
-                  <MobileNavBar />
-                </Suspense>
-                <ReactQueryDevtools initialIsOpen={false} />
-              </SharedStateProvider>
-            </QueryProvider>
-            <GitHubButton />
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <div className="flex flex-col h-[100dvh] w-full overflow-hidden">
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              <QueryProvider>
+                <SharedStateProvider
+                  availableSemesters={availableSemesters}
+                  defaultTeachingSemester={latestSemester}
+                >
+                  <div className="flex-1 overflow-y-auto overscroll-y-contain relative">
+                    {children}
+                  </div>
+                  <Suspense fallback={null}>
+                    <MobileNavBar />
+                  </Suspense>
+                  <ReactQueryDevtools initialIsOpen={false} />
+                </SharedStateProvider>
+              </QueryProvider>
+              <GitHubButton />
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </div>
       </body>
     </html>
   );
