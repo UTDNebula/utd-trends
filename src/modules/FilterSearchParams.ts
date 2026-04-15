d2b747be410a40093054b998f5f016bcc0bc915f
Isoscelestial <isoscelestial@gmail.com>
2026-04-15 17:09:39 -0500

    chore: update searchParams and FilterSearchParams modules

--- src/modules/FilterSearchParams.ts
+++ src/modules/FilterSearchParams.ts
@@ -3,74 +3,96 @@ type KeyAsString<T> = Extract<keyof T, string>;
 type IndexValueAsString<T extends Record<string, unknown>> =
   T[KeyAsString<T>] extends string ? T[KeyAsString<T>] : string;
 
-interface FilterSearchParamsOptions<K, V> {
-  onAppend?: (name: K, value: V) => void;
-  onDelete?: (name: K, value?: V) => void;
-  onSet?: (name: K, value: V) => void;
+interface FilterSearchParamsOptions<
+  K = Record<string, unknown>,
+  V = Record<string, unknown>,
+> {
+  onAppend?: (name: K, value: V, rawParams: URLSearchParams) => void;
+  onDelete?: (
+    name: K,
+    value: V | undefined,
+    rawParams: URLSearchParams,
+  ) => void;
+  onSet?: (name: K, value: V, rawParams: URLSearchParams) => void;
 }
+
+export type FilterSearchParamsSchema = Record<string, unknown>;
+
+export type FilterSearchParamsOptionsWithSchema<
+  Schema extends FilterSearchParamsSchema,
+> = FilterSearchParamsOptions<KeyAsString<Schema>, IndexValueAsString<Schema>>;
 
 /**
  * Wrapper class for {@linkcode URLSearchParams} that adds type safety to
  * methods and a constructor that accepts callback functions for when
  * {@linkcode append}, {@linkcode delete}, or {@linkcode set} are used.
  */
-export class FilterSearchParams<
-  T extends Record<string, unknown> = Record<string, unknown>,
+export default class FilterSearchParams<
+  Schema extends FilterSearchParamsSchema,
 > extends URLSearchParams {
-  private options: FilterSearchParamsOptions<
-    KeyAsString<T>,
-    IndexValueAsString<T>
-  > = {};
+  private options: FilterSearchParamsOptionsWithSchema<Schema> = {};
+
+  /**
+   * Proxy to allow callbacks to safely call the raw methods of URLSearchParams without infinite recursion
+   */
+  private get safeInstance(): URLSearchParams {
+    return new Proxy(this, {
+      get(target, prop) {
+        const value = Reflect.get(URLSearchParams.prototype, prop, target);
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+    });
+  }
 
   constructor(
     init:
       | string
       | URLSearchParams
       | string[][]
-      | Record<keyof T, string>
+      | Record<keyof Schema, string>
       | undefined,
-    options?: FilterSearchParamsOptions<KeyAsString<T>, IndexValueAsString<T>>,
+    options?: FilterSearchParamsOptionsWithSchema<Schema>,
   ) {
     super(init);
     if (options) this.options = options;
   }
 
   append<
-    K extends KeyAsString<T>,
-    V extends T[K] extends string ? T[K] : string,
+    K extends KeyAsString<Schema>,
+    V extends Schema[K] extends string ? Schema[K] : string,
   >(name: K, value: V): void {
     super.append(name, value);
-    this.options.onAppend?.(name, value);
+    this.options.onAppend?.(name, value, this.safeInstance);
   }
 
   delete<
-    K extends KeyAsString<T>,
-    V extends T[K] extends string ? T[K] : string,
+    K extends KeyAsString<Schema>,
+    V extends Schema[K] extends string ? Schema[K] : string,
   >(name: K, value?: V): void {
     super.delete(name, value);
-    this.options.onDelete?.(name, value);
+    this.options.onDelete?.(name, value, this.safeInstance);
   }
 
-  get(name: KeyAsString<T>): string | null {
+  get(name: KeyAsString<Schema>): string | null {
     return super.get(name);
   }
 
-  getAll(name: KeyAsString<T>): string[] {
+  getAll(name: KeyAsString<Schema>): string[] {
     return super.getAll(name);
   }
 
-  has<K extends KeyAsString<T>, V extends T[K] extends string ? T[K] : string>(
-    name: K,
-    value?: V,
-  ): boolean {
+  has<
+    K extends KeyAsString<Schema>,
+    V extends Schema[K] extends string ? Schema[K] : string,
+  >(name: K, value?: V): boolean {
     return super.has(name, value);
   }
 
-  set<K extends KeyAsString<T>, V extends T[K] extends string ? T[K] : string>(
-    name: K,
-    value: V,
-  ): void {
+  set<
+    K extends KeyAsString<Schema>,
+    V extends Schema[K] extends string ? Schema[K] : string,
+  >(name: K, value: V): void {
     super.set(name, value);
-    this.options.onSet?.(name, value);
+    this.options.onSet?.(name, value, this.safeInstance);
   }
 }
--- src/modules/searchParams.ts
+++ src/modules/searchParams.ts
@@ -1,4 +1,7 @@
-import { FilterSearchParams } from './FilterSearchParams';
+import FilterSearchParams, {
+  type FilterSearchParamsOptionsWithSchema,
+  type FilterSearchParamsSchema,
+} from './FilterSearchParams';
 
 /**
  * Set search params in the URL without using Next.js's Router
@@ -22,24 +25,48 @@ export const navigateWithParams = (
  * Modifies the URL search params based off {@linkcode setParamsFn} and navigates to it
  * @param setParamsFn Function to that accepts a mutable params parameter and modifies it
  * @param pathname Optional pathname to override the default, which uses `window.location.pathname`
+ * @param options Optional options for the {@linkcode FilterSearchParams} object created at runtime
  */
 export const setParams = <
-  Schema extends Record<string, unknown> = Record<string, unknown>,
+  Schema extends FilterSearchParamsSchema = FilterSearchParamsSchema,
 >(
   setParamsFn: (params: FilterSearchParams<Schema>) => void,
   pathname: string = window.location.pathname,
+  options: FilterSearchParamsOptionsWithSchema<Schema> = {},
 ) => {
-  const params = new FilterSearchParams<Schema>(window.location.search, {
-    onAppend(name, value) {
-      console.log(`APPEND ${name} WITH '${value}'`);
-    },
-    onDelete(name, value) {
-      console.log(`DELETE ${name}${value ? ` IF '${value}'` : ''}`);
-    },
-    onSet(name, value) {
-      console.log(`SET ${name} TO '${value}'`);
-    },
-  });
+  const params = new FilterSearchParams<Schema>(
+    window.location.search,
+    options,
+  );
   setParamsFn(params);
   navigateWithParams(pathname, params);
 };
+
+/**
+ * Creates an object with a {@linkcode ParamSetter.setParams | setParams} method.
+ * The method will automatically receive this {@linkcode ParamSetter} object's
+ * {@linkcode options} and {@linkcode Schema} type.
+ *
+ * @template Schema
+ * @param options Optional options for the {@linkcode FilterSearchParams} object created at runtime
+ */
+export class ParamSetter<Schema extends FilterSearchParamsSchema> {
+  private options: FilterSearchParamsOptionsWithSchema<Schema> = {};
+
+  constructor(options?: FilterSearchParamsOptionsWithSchema<Schema>) {
+    if (options) this.options = options;
+    this.setParams = this.setParams.bind(this);
+  }
+
+  /**
+   * Modifies the URL search params based off {@linkcode setParamsFn} and navigates to it
+   * @param setParamsFn Function to that accepts a mutable params parameter and modifies it
+   * @param pathname Optional pathname to override the default, which uses `window.location.pathname`
+   */
+  setParams(
+    setParamsFn: (params: FilterSearchParams<Schema>) => void,
+    pathname: string = window.location.pathname,
+  ) {
+    setParams<Schema>(setParamsFn, pathname, this.options);
+  }
+}
