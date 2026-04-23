1edd98cba1933cc7f5bf2a6b7374f8d58a59dbc8
Isoscelestial <isoscelestial@gmail.com>
2026-04-23 18:07:57 -0500

    refactor: use factory method for creating a ParamSetter

--- src/modules/searchParams.ts
+++ src/modules/searchParams.ts
@@ -6,10 +6,7 @@ import FilterSearchParams, {
 /**
  * Set search params in the URL without using Next.js's Router
  */
-export const navigateWithParams = (
-  pathname: string,
-  params: URLSearchParams,
-) => {
+export function navigateWithParams(pathname: string, params: URLSearchParams) {
   // Manually replace page URL to avoid triggering re-renders with Next.js's hooks
   window.history.replaceState(
     null,
@@ -19,54 +16,101 @@ export const navigateWithParams = (
 
   // Manually indicate the page's URL has changed, which will update Next.js's hooks
   window.dispatchEvent(new PopStateEvent('popstate'));
-};
+}
 
 /**
  * Modifies the URL search params based off {@linkcode setParamsFn} and navigates to it
  * @param setParamsFn Function to that accepts a mutable params parameter and modifies it
- * @param pathname Optional pathname to override the default, which uses `window.location.pathname`
+ * @param pathname Optional pathname to override the default. Use `null` for default, which is `window.location.pathname`.
  * @param options Optional options for the {@linkcode FilterSearchParams} object created at runtime
+ *
+ * @example
+ * <caption>Basic usage</caption>
+ * setParams((params) => {
+ *   params.set("foo", "bar")
+ * })
+ *
+ * @example
+ * <caption>With schema</caption>
+ * type Schema = { foo: string };
+ * setParams<Schema>((params) => {
+ *   params.set("foo", "bar");
+ * });
+ *
+ * @example
+ * <caption>With options</caption>
+ * setParams(
+ *   (params) => {
+ *     params.set("foo", "bar");
+ *   },
+ *   null,
+ *   {
+ *     onSet(name, value) {
+ *       console.log(`Set ${name} to ${value}!`);
+ *     },
+ *   },
+ * );
  */
-export const setParams = <
+export function setParams<
   Schema extends FilterSearchParamsSchema = FilterSearchParamsSchema,
 >(
   setParamsFn: (params: FilterSearchParams<Schema>) => void,
-  pathname: string = window.location.pathname,
+  pathname: string | null = null,
   options: FilterSearchParamsOptionsWithSchema<Schema> = {},
-) => {
+) {
   const params = new FilterSearchParams<Schema>(
     window.location.search,
     options,
   );
   setParamsFn(params);
+  if (pathname === null) pathname = window.location.pathname;
   navigateWithParams(pathname, params);
-};
+}
 
 /**
- * Creates an object with a {@linkcode ParamSetter.setParams | setParams} method.
- * The method will automatically receive this {@linkcode ParamSetter} object's
- * {@linkcode options} and {@linkcode Schema} type.
+ * Factory method to create a {@linkcode setParams} method that will
+ * automatically receive the {@linkcode options} and {@linkcode Schema} type.
  *
- * @template Schema
+ * @template Schema Key-value pair object, corresponding to name-value pair relationship of URLSearchParams
  * @param options Optional options for the {@linkcode FilterSearchParams} object created at runtime
+ * @returns A higher-order {@linkcode setParams} method
+ *
+ * @example
+ * <caption>With schema and options</caption>
+ * type Schema = { foo: string };
+ * export const setTypedParams = createParamSetter<Schema>({
+ *   onSet(name, value) {
+ *     console.log(`Set ${name} to ${value}!`)
+ *   }
+ * })
+ *
+ * // In another file
+ * setTypedParams((params) => {
+ *   params.set("foo", "bar")
+ * })
  */
-export class ParamSetter<Schema extends FilterSearchParamsSchema> {
-  private options: FilterSearchParamsOptionsWithSchema<Schema> = {};
-
-  constructor(options?: FilterSearchParamsOptionsWithSchema<Schema>) {
-    if (options) this.options = options;
-    this.setParams = this.setParams.bind(this);
-  }
+export function createParamSetter<Schema extends FilterSearchParamsSchema>(
+  options?: FilterSearchParamsOptionsWithSchema<Schema>,
+) {
+  const factoryOptions = options;
 
   /**
    * Modifies the URL search params based off {@linkcode setParamsFn} and navigates to it
    * @param setParamsFn Function to that accepts a mutable params parameter and modifies it
-   * @param pathname Optional pathname to override the default, which uses `window.location.pathname`
+   * @param pathname Optional pathname to override the default. Use `null` for default, which is `window.location.pathname`.
+   * @param options Options object that is shallow-merged with the options object provided to {@linkcode createParamSetter}
+   *
+   * @example
+   * <caption>Basic usage</caption>
+   * setTypedParams((params) => {
+   *   params.set("foo", "bar")
+   * })
    */
-  setParams(
+  return function (
     setParamsFn: (params: FilterSearchParams<Schema>) => void,
-    pathname: string = window.location.pathname,
+    pathname: string | null = null,
+    options?: FilterSearchParamsOptionsWithSchema<Schema>,
   ) {
-    setParams<Schema>(setParamsFn, pathname, this.options);
-  }
+    setParams<Schema>(setParamsFn, pathname, { ...factoryOptions, ...options });
+  };
 }
